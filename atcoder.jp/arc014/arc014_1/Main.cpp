b640fa2b5f26f4bb797a41b41144142949b74b7d
okura <okuraofvegetable@gmail.com>
2013-06-16 22:05:06 +0900

    [AC] arc014 arc014_1

---
+++ atcoder.jp/arc014/arc014_1/Main.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+typedef pair<int,int> P;
+#define pu push
+#define pb push_back
+#define mp make_pair
+#define eps 1e-7
+#define INF 2000000000
+int main()
+{
+	int n;
+	cin >> n;
+	if(n%2==1)cout << "Red" << endl;
+	else cout << "Blue" << endl;
+	return 0;
+}
