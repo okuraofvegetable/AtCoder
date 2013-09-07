7c3f6541ee8456468451a26adf5cf0062066d0df
okura <okuraofvegetable@gmail.com>
2013-09-07 22:11:12 +0900

    [AC] language-test-ver1 test001_b

---
+++ atcoder.jp/language-test-ver1/test001_b/Main.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pu push
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define SORT(x) sort((x).begin(),(x).end())
+#define all(x) (x).begin(),(x).end()
+#define EQ(a,b) (abs((a)-(b))<EPS)
+int main()
+{
+	string x;
+	cin >> x;
+	string s;
+	cin >> s;
+	string ans;
+	for(int i=0;i<s.size();i++)
+	{
+		if(s[i]!=x[0])ans+=s[i];
+	}
+	cout << ans << endl;
+	return 0;
+}
