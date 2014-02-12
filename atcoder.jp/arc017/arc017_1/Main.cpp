838e655f70d396c6eb10f7a439388c0d951ba533
okura <okuraofvegetable@gmail.com>
2014-02-12 23:12:09 +0900

    [AC] arc017 arc017_1

---
+++ atcoder.jp/arc017/arc017_1/Main.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pu push
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define SORT(x) sort((x).begin(),(x).end())
+#define all(x) (x).begin(),(x).end()
+#define EQ(a,b) (abs((a)-(b))<EPS)
+int main()
+{
+	int n;
+	cin >> n;
+	bool flag=false;
+	for(int i=2;i<=n/2;i++)if(n%i==0){flag=true;break;}
+	if(flag)cout << "NO" << endl;
+	else cout << "YES" << endl;
+	return 0;
+}
