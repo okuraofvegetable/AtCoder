7cc38e544f3e38e716919979392d805f31efd1ba
okura <okuraofvegetable@gmail.com>
2014-04-19 21:03:32 +0900

    [AC] abc007 abc007_2

---
+++ atcoder.jp/abc007/abc007_2/Main.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pu push
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define SORT(x) sort((x).begin(),(x).end())
+#define all(x) (x).begin(),(x).end()
+#define EQ(a,b) (abs((a)-(b))<EPS)
+int main()
+{
+string s;
+cin >> s;
+if(s=="a"){cout << -1 << endl;return 0;}
+if(s.size()==1){cout << char(s[0]-1) << endl;}
+else
+{
+for(int i=0;i<s.size()-1;i++)cout << s[i];
+cout << endl;
+}
+        return 0;
+}       
