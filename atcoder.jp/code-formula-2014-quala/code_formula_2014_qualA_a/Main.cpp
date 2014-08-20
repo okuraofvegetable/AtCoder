fecd0aca40a1806c7b585968aeb7efe1a78f2522
okura <okuraofvegetable@gmail.com>
2014-08-20 20:09:54 +0900

    [AC] code-formula-2014-quala code_formula_2014_qualA_a

---
+++ atcoder.jp/code-formula-2014-quala/code_formula_2014_qualA_a/Main.cpp
@@ -0,0 +1,47 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pu push
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define SORT(x) sort((x).begin(),(x).end())
+#define all(x) (x).begin(),(x).end()
+#define EQ(a,b) (abs((a)-(b))<eps)
+int main()
+{
+	int a,b,c;
+	string s;
+	cin >> a;
+	for(int i=0;i<=100;i++)
+	{
+		if(i*i*i==a)
+		{
+			cout << "YES" << endl;
+			return 0;
+		}
+	}
+	cout << "NO" << endl;
+	return 0;
+}
