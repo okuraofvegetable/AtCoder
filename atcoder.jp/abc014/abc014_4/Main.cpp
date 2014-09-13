37dbbdde82b0e28985c158cd9d68ce6c4bb7ae76
okura <okuraofvegetable@gmail.com>
2014-09-13 21:14:00 +0900

    [AC] abc014 abc014_4

---
+++ atcoder.jp/abc014/abc014_4/Main.cpp
@@ -0,0 +1,104 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pu push
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define SORT(x) sort((x).begin(),(x).end())
+#define all(x) (x).begin(),(x).end()
+#define EQ(a,b) (abs((a)-(b))<EPS)
+#define MAX_N 100000
+#define MAX_LOG_V 20
+vector<int> g[MAX_N];
+int N,Q,root=0;
+int parent[MAX_LOG_V][MAX_N];
+int depth[MAX_N];
+void dfs(int v,int p,int d)
+{
+	depth[v]=d;
+	parent[0][v]=p;
+	for(int i=0;i<g[v].size();i++)
+	{
+		if(g[v][i]!=p)
+		{
+			dfs(g[v][i],v,d+1);
+		}
+	}
+	return;
+}
+void init()
+{
+	dfs(root,-1,0);
+	for(int i=0;i<MAX_LOG_V-1;i++)
+	{
+		for(int v=0;v<N;v++)
+		{
+			if(parent[i][v]==-1)parent[i+1][v]=-1;
+			else parent[i+1][v]=parent[i][parent[i][v]];
+		}
+	}
+	return;
+}
+int lca(int u,int v)
+{
+	if(depth[u]>depth[v])swap(u,v);
+	for(int i=19;i>=0;i--)
+	{
+		if(depth[v]-depth[u]>>i)v=parent[i][v];
+	}
+	if(u==v)return u;
+	for(int i=19;i>=0;i--)
+	{
+		if(parent[i][u]!=parent[i][v])
+		{
+			u=parent[i][u];
+			v=parent[i][v];
+		}
+	}
+	return parent[0][u];
+}
+int main()
+{
+	scanf("%d",&N);
+	for(int i=0;i<N-1;i++)
+	{
+		int a,b;
+		scanf("%d %d",&a,&b);
+		a--;b--;
+		g[a].pb(b);
+		g[b].pb(a);
+	}
+	init();
+	scanf("%d",&Q);
+	for(int i=0;i<Q;i++)
+	{
+		int u,v;
+		scanf("%d %d",&u,&v);
+		u--;v--;
+		int k=lca(u,v);
+		printf("%d\n",(depth[u]+depth[v]-depth[k]*2+1));
+	}
+	return 0;
+}
