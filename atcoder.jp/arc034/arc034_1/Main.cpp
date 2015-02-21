95dbdaa7cf3b367b3572ce629ec95357161211a0
okura <okuraofvegetable@gmail.com>
2015-02-21 21:04:18 +0900

    [AC] arc034 arc034_1

---
+++ atcoder.jp/arc034/arc034_1/Main.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <iomanip>
+#include <set>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pu push
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define SORT(x) sort((x).begin(),(x).end())
+#define all(x) (x).begin(),(x).end()
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+int main()
+{
+	int N;
+	cin >> N;
+	vector<double> v;
+	rep(i,N)
+	{
+		double a;
+		double p = 0;
+		rep(j,4)
+		{
+			cin >> a;
+			p+=a;
+		}
+		cin >> a;
+		p += a*110.0/900.0;
+		v.pb(p);
+	}
+	sort(all(v),greater<double>());
+	cout << setprecision(16) << v[0] << endl;
+	return 0;
+}
