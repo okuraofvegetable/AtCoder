ec4809ef6f47cc66721a0d95352f935181aa5761
okura <okuraofvegetable@gmail.com>
2015-03-26 21:59:34 +0900

    [AC] indeednow-finala-open indeednow_2015_finala_a

---
+++ atcoder.jp/indeednow-finala-open/indeednow_2015_finala_a/Main.cpp
@@ -0,0 +1,47 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pu push
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define SORT(x) sort((x).begin(),(x).end())
+#define all(x) (x).begin(),(x).end()
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	vector<int> t(n/2);
+	for(int i=0;i<n;i++)cin >> v[i];
+	sort(all(v));
+	for(int i=0;i<n/2;i++)t[i]=v[i]+v[n-1-i];
+	sort(all(t));
+	cout << t[n/2-1]-t[0] << endl;
+	return 0;
+}
