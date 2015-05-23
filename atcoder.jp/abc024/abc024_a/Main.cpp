2a486371d7be40435a1ea1dbf229cee3aafc4a20
okura <okuraofvegetable@gmail.com>
2015-05-23 21:02:10 +0900

    [AC] abc024 abc024_a

---
+++ atcoder.jp/abc024/abc024_a/Main.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pu push
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define SORT(x) sort((x).begin(),(x).end())
+#define all(x) (x).begin(),(x).end()
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+int main()
+{
+	int A,B,C,K;
+	int S,T;
+	cin >> A >> B >> C >> K;
+	cin >> S >> T;
+	int ans = A*S+B*T;
+	if(S+T>=K)ans -= C*(S+T);
+	cout << ans << endl;
+	return 0;
+}
