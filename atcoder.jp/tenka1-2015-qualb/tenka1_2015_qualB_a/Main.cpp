fcb7c093fe1a28984c6e55f2329fc770656ca030
okura <okuraofvegetable@gmail.com>
2015-08-22 21:09:43 +0900

    [AC] tenka1-2015-qualb tenka1_2015_qualB_a

---
+++ atcoder.jp/tenka1-2015-qualb/tenka1_2015_qualB_a/Main.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pu push
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define SORT(x) sort((x).begin(),(x).end())
+#define all(x) (x).begin(),(x).end()
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+ll a[22];
+int main()
+{
+	a[0]=100ll;
+	a[1]=100ll;
+	a[2]=200ll;
+	for(int i=3;i<20;i++)a[i]=a[i-1]+a[i-2]+a[i-3];
+	cout << a[19] << endl;
+	return 0;
+}
