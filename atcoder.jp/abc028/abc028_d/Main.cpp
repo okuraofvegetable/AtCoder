6ca3c0b2ef4d32db8e5baf618ddebd11555b916f
okura <okuraofvegetable@gmail.com>
2015-08-29 21:14:51 +0900

    [AC] abc028 abc028_d

---
+++ atcoder.jp/abc028/abc028_d/Main.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
+#include <iomanip>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pu push
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define SORT(x) sort((x).begin(),(x).end())
+#define all(x) (x).begin(),(x).end()
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+int N,K;
+int main()
+{
+	cin >> N >> K;
+	double n = N;
+	double k = K;
+	cout << setprecision(16) <<(6.0*(k-1.0)*(n-k)+3.0*(n-1)+1.0)/n/n/n << endl;
+	return 0;
+}
