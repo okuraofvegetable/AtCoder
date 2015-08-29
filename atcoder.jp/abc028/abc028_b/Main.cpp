fe8bdea2daef846b4878348bbbc62d41099e7e62
okura <okuraofvegetable@gmail.com>
2015-08-29 21:06:55 +0900

    [AC] abc028 abc028_b

---
+++ atcoder.jp/abc028/abc028_b/Main.cpp
@@ -0,0 +1,48 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pu push
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define SORT(x) sort((x).begin(),(x).end())
+#define all(x) (x).begin(),(x).end()
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+int cnt[10];
+int main()
+{
+	string s;
+	cin >> s;
+	for(int i=0;i<s.size();i++)cnt[s[i]-'A']++;
+	for(int i=0;i<6;i++)
+	{
+		cout << cnt[i];
+		if(i<5)cout << ' ';
+	}
+	cout << endl;
+	return 0;
+}
