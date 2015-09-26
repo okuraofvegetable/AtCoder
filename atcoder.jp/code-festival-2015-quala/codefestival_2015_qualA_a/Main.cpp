1468f9fa3f3b15ea532a505bee484397eb786fa0
okura <okuraofvegetable@gmail.com>
2015-09-26 21:07:41 +0900

    [AC] code-festival-2015-quala codefestival_2015_qualA_a

---
+++ atcoder.jp/code-festival-2015-quala/codefestival_2015_qualA_a/Main.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pu push
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define SORT(x) sort((x).begin(),(x).end())
+#define all(x) (x).begin(),(x).end()
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+int main()
+{
+	string s;
+	cin >> s;
+	int len = s.size();
+	s[len-1]='5';
+	cout << s << endl;
+	return 0;
+}
