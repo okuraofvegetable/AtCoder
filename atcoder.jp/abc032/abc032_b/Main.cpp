5077cab5688a1e1e111a7a2c8c18be5ef1e8894a
okura <okuraofvegetable@gmail.com>
2016-01-09 22:21:05 +0900

    [AC] abc032 abc032_b

---
+++ atcoder.jp/abc032/abc032_b/Main.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pu push
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define SORT(x) sort((x).begin(),(x).end())
+#define all(x) (x).begin(),(x).end()
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+int main(){
+	string s;
+	cin >> s;
+	int k;
+	cin >> k;
+	set<string> se;
+	for(int i=0;i<=sz(s)-k;i++){
+		se.insert(s.substr(i,k));
+	}
+	cout << sz(se) << endl;
+	return 0;
+}
