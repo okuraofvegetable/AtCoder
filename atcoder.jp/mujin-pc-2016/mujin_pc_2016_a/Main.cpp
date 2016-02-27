bdf6067aaa20b972e5a9624b22137539abdac862
okura <okuraofvegetable@gmail.com>
2016-02-27 21:01:38 +0900

    [AC] mujin-pc-2016 mujin_pc_2016_a

---
+++ atcoder.jp/mujin-pc-2016/mujin_pc_2016_a/Main.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+char c;
+int main(){
+	scanf(" %c",&c);
+	if(c=='O'||c=='P'||c=='K'||c=='L'){
+		printf("Right\n");
+	}else{
+		printf("Left\n");
+	}
+	return 0;
+}
