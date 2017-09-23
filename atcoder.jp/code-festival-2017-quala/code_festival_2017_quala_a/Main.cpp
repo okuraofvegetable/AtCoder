f23d5c8294554414cc5ec5d5b3cdbf1556b520e6
okura <okuraofvegetable@gmail.com>
2017-09-23 21:01:36 +0900

    [AC] code-festival-2017-quala code_festival_2017_quala_a

---
+++ atcoder.jp/code-festival-2017-quala/code_festival_2017_quala_a/Main.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int main(){
+	string s;
+	cin >> s;
+	if(s.size()>=4 && s.substr(0,4)==string("YAKI"))cout << "Yes" << endl;
+	else cout << "No" << endl;
+	return 0;
+}
