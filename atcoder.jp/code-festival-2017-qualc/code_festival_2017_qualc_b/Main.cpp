8ef7bc641c82b5b57699a43c3a2fe9f7711bd144
okura <okuraofvegetable@gmail.com>
2017-11-03 06:01:33 +0900

    [AC] code-festival-2017-qualc code_festival_2017_qualc_b

---
+++ atcoder.jp/code-festival-2017-qualc/code_festival_2017_qualc_b/Main.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int N;
+int A[14];
+int main(){
+	cin >> N;
+	int ret = 1,al = 1;
+	for(int i=0;i<N;i++){
+		cin >> A[i];
+		if(A[i]%2==0)ret*=2;
+		al*=3;
+	}
+	cout << al-ret << endl;
+	return 0;
+}
