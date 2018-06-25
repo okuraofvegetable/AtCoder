21ab17df65ad7f18a0de55327ba6bc72788ef02b
okura <okuraofvegetable@gmail.com>
2018-06-25 17:54:37 +0900

    [AC] arc064 arc064_b

---
+++ atcoder.jp/arc064/arc064_b/Main.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 1000000000
+#define LLINF 1000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+#define MOD 1000000007ll
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+string s;
+int main(){
+	cin >> s;
+	int len = s.length();
+	if(s[0]==s[len-1]){
+		if(len%2==1)cout << "Second" << endl;
+		else cout << "First" << endl;
+	}else{
+		if(len%2==1)cout << "First" << endl;
+		else cout << "Second" << endl;
+	}
+	return 0;
+}
