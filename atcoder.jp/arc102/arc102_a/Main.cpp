fd9976c41020c07484419aa4e6387c6125d90953
okura <okuraofvegetable@gmail.com>
2018-09-01 21:13:26 +0900

    [AC] arc102 arc102_a

---
+++ atcoder.jp/arc102/arc102_a/Main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef pair<P,int> T;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define LLINF 1000000000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int N,K;
+ll ans;
+int main(){
+	scanf("%d %d",&N,&K);
+	if(K%2==1){
+		ll k = N/K;
+		cout << k*k*k << endl;
+	}else{
+		ll k = N/(K/2);
+		ll even = k/2;
+		ll odd = k-even;
+		cout << odd*odd*odd+even*even*even << endl;
+	}
+	return 0;
+}
