951edd95ee22285be9d702b0fd8a22a4cf4656ab
okura <okuraofvegetable@gmail.com>
2018-11-23 21:01:04 +0900

    [AC] ddcc2019-qual ddcc2018_qual_a

---
+++ atcoder.jp/ddcc2019-qual/ddcc2018_qual_a/Main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define LLINF 100000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int N;
+int main(){
+	cin >> N;
+	int ans = 1;
+	for(int i=0;i<N;i++)ans*=4;
+		cout  << ans << endl;
+	return 0;
+}
