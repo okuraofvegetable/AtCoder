9a421bd665f909c94e8e283586ccd8cbe80adbd0
okura <okuraofvegetable@gmail.com>
2018-11-25 12:13:05 +0900

    [AC] code-thanks-festival-2018 code_thanks_festival_2018_d

---
+++ atcoder.jp/code-thanks-festival-2018/code_thanks_festival_2018_d/Main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define LLINF 100000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+string s;
+int main(){
+	cin >> s;
+	char st = s[0];
+	int ans = 0;
+	for(int i=0;i<s.size();i++){
+		if(st>=s[i]){
+			ans++;
+			st = s[i];
+			//cout << st << endl;
+		}
+	}
+	cout << ans << endl;
+	return 0;
+}
