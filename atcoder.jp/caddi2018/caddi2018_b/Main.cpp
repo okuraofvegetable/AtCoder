05fd3067611ec98da79d51017860df78aa95b2a1
okura <okuraofvegetable@gmail.com>
2018-12-22 22:02:04 +0900

    [AC] caddi2018 caddi2018_b

---
+++ atcoder.jp/caddi2018/caddi2018_b/Main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,int> P;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define LLINF 1000000000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int n;
+int a[100100];
+int main(){
+	cin >> n;
+	bool flag = false;
+	for(int i=0;i<n;i++){
+		cin >> a[i];
+		if(a[i]%2==1)flag = true;
+	}
+	if(flag)cout << "first" << endl;
+	else cout << "second" << endl;
+	return 0;
+}
