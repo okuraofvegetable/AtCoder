b5a79f00f6919ed1a9377c8c86affb130305ac96
okura <okuraofvegetable@gmail.com>
2019-01-12 21:02:03 +0900

    [AC] aising2019 aising2019_a

---
+++ atcoder.jp/aising2019/aising2019_a/Main.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define LLINF 1000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int N,H,W;
+int main(){
+	cin >> N;
+	cin >> H;
+	cin >> W;
+	printf("%d\n",(N-H+1)*(N-W+1));
+	return 0;
+}
