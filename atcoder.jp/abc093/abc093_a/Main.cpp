5d4b806fa1d429c2493daf4dc2e30c5f76629533
okura <okuraofvegetable@gmail.com>
2019-02-14 22:45:39 +0900

    [AC] abc093 abc093_a

---
+++ atcoder.jp/abc093/abc093_a/Main.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define LLINF 1000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int cnt[5];
+int main(){
+	string s;
+	cin >> s;
+	for(int i=0;i<s.size();i++)cnt[s[i]-'a']++;
+	for(int i=0;i<3;i++){
+		if(cnt[i]!=1){
+			cout << "No" << endl;
+			return 0;
+		}
+	}
+	cout << "Yes" << endl;
+	return 0;
+}
