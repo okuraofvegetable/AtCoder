5b1d1993ebf71feb998b38bb06579e15598d3ad9
okura <okuraofvegetable@gmail.com>
2019-02-19 22:13:17 +0900

    [AC] abc118 abc118_a

---
+++ atcoder.jp/abc118/abc118_a/Main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<ll,ll> P;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 1000000000
+#define LLINF 1000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int main(){
+	int A,B;
+	cin >> A >> B;
+	if(B%A==0)cout << A+B << endl;
+	else cout << B-A << endl;
+	return 0;
+}
