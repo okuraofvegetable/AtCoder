cb9884ece22fe29a413aff856fa5e72a2fb83d29
okura <okuraofvegetable@gmail.com>
2019-02-27 13:23:51 +0900

    [AC] abc119 abc119_b

---
+++ atcoder.jp/abc119/abc119_b/Main.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define LLINF 1000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int main(){
+	int N;
+	cin >> N;
+	double ans = 0.0;
+	for(int i=0;i<N;i++){
+		string s;
+		double x;
+		cin >> x >> s;
+		if(s=="JPY")ans += x;
+		else ans += 380000.0*x;
+	}
+	cout << setprecision(16) << ans << endl;
+	return 0;
+}
