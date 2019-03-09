70173491a2478f2b6884b55124a76e4c528bafbd
okura <okuraofvegetable@gmail.com>
2019-03-10 06:29:16 +0900

    [AC] agc001 agc001_a

---
+++ atcoder.jp/agc001/agc001_a/Main.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,int> P;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 1000000000
+#define LLINF 1000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int N;
+int main(){
+	cin >> N;
+	vector<int> L(2*N);
+	for(int i=0;i<2*N;i++)cin >> L[i];
+	sort(all(L));
+	int ans = 0;
+	for(int i=0;i<N;i++)ans+=L[i*2];
+	cout << ans << endl;
+	return 0;
+}
