0d8c81123c5dd8bde4c60884b805417a8ca9486f
okura <okuraofvegetable@gmail.com>
2019-03-12 22:31:58 +0900

    [AC] agc004 agc004_a

---
+++ atcoder.jp/agc004/agc004_a/Main.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,int> P;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 1000000000
+#define LLINF 1000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+ll solve(ll a,ll b,ll c){
+	if(a%2ll==0ll)return 0ll;
+	else return b*c;
+}
+int main(){
+	ll a,b,c;
+	cin >> a >> b >> c;
+	cout << min(min(solve(a,b,c),solve(b,c,a)),solve(c,a,b)) << endl;
+	return 0;
+}
