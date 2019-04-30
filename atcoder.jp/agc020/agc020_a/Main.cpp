fee24001e9be79c8fe27786b5ef1227baa52fa36
okura <okuraofvegetable@gmail.com>
2019-04-30 13:17:44 +0900

    [AC] agc020 agc020_a

---
+++ atcoder.jp/agc020/agc020_a/Main.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int N,A,B;
+	cin >> N >> A >> B;
+	if(abs(A-B)%2==1)cout << "Borys" << endl;
+	else cout << "Alice" << endl;
+	return 0;
+}
