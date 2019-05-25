a2efcf3643214594c8b8e0f2c0500cec69441faf
okura <okuraofvegetable@gmail.com>
2019-05-25 21:20:48 +0900

    [AC] abc127 abc127_b

---
+++ atcoder.jp/abc127/abc127_b/Main.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef pair<P,int> T;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int main(){
+	ll r,d,x;
+	cin >> r >> d >> x;
+	for(int i=1;i<=10;i++){
+		x = x*r-d;
+		cout << x << endl;
+	}
+	return 0;
+}
