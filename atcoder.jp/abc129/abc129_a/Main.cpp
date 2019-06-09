11108afde731a56678e4fc1ee37b8d9de4ac5ce7
okura <okuraofvegetable@gmail.com>
2019-06-09 21:12:00 +0900

    [AC] abc129 abc129_a

---
+++ atcoder.jp/abc129/abc129_a/Main.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef pair<P,int> T;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int AB,BC,CA;
+int main(){
+	cin >> AB >> BC >> CA;
+	vector<int> vec;
+	vec.pb(CA+BC);
+	vec.pb(AB+BC);
+	vec.pb(AB+CA);
+	int ans = INF;
+	for(int i=0;i<vec.size();i++){
+		ans = min(ans,vec[i]);
+	}
+	cout << ans << endl;
+	return 0;
+}
