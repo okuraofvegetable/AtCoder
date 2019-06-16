a00699703a7deed28aa0f981de95628a99b8b299
okura <okuraofvegetable@gmail.com>
2019-06-16 21:55:35 +0900

    [AC] abc130 abc130_b

---
+++ atcoder.jp/abc130/abc130_b/Main.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef pair<P,int> T;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int N;
+int L[105];
+int X;
+int main(){
+    cin >> N >> X;
+    for(int i=1;i<=N;i++)cin >> L[i];
+    for(int i=1;i<=N;i++)L[i]+=L[i-1];
+    int ans = 0;
+    for(int i=0;i<=N;i++){
+        if(L[i]<=X)ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
