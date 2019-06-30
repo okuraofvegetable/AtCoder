78b8229d30ff4588f9e8f0da33947da86e7902ed
okura <okuraofvegetable@gmail.com>
2019-06-30 11:32:41 +0900

    [AC] abc132 abc132_a

---
+++ atcoder.jp/abc132/abc132_a/Main.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef pair<P,int> T;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int cnt[35];
+int main(){
+	string s;
+	cin >> s;
+	for(int i=0;i<s.size();i++){
+		cnt[s[i]-'A']++;
+	}
+	for(int i=0;i<30;i++){
+		if(!(cnt[i]==0||cnt[i]==2)){
+			cout << "No" << endl;
+			return 0;
+		}
+	}
+	cout << "Yes" << endl;
+	return 0;
+}
