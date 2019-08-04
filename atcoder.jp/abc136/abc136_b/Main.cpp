6529698294a937a0e7f05a7084faa9221afd32ca
okura <okuraofvegetable@gmail.com>
2019-08-04 21:39:42 +0900

    [AC] abc136 abc136_b

---
+++ atcoder.jp/abc136/abc136_b/Main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef pair<P,int> T;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int dig(int x){
+	int res = 0;
+	while(x){
+		x /= 10;
+		res++;
+	}
+	return res;
+}
+int main(){
+	int N;
+	cin >> N;
+	int ans = 0;
+	for(int i=1;i<=N;i++){
+		if(dig(i)%2==1)ans++;
+	}
+	cout << ans << endl;
+	return 0;
+}
