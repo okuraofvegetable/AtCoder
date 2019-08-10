88ad010eb6a2d59719174d8292c50d55692f82ee
okura <okuraofvegetable@gmail.com>
2019-08-10 21:48:20 +0900

    [AC] abc137 abc137_b

---
+++ atcoder.jp/abc137/abc137_b/Main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef pair<P,int> T;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int K,X;
+int main(){
+	cin >> K >> X;
+	for(int i=max(-1000000,X-K+1);i<=min(1000000,X+K-1);i++){
+		cout << i;
+		if(i==min(1000000,X+K-1))cout << endl;
+		else cout << ' ';
+	}
+	return 0;
+}
