3b29f5cd7dbed6f814483e674e9c1947abd509f2
okura <okuraofvegetable@gmail.com>
2019-08-11 16:10:46 +0900

    [AC] abc135 abc135_a

---
+++ atcoder.jp/abc135/abc135_a/Main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef pair<P,int> T;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int main(){
+	int A,B;
+	cin >> A >> B;
+	if((A+B)%2==0)cout << (A+B)/2 << endl;
+	else cout << "IMPOSSIBLE" << endl;
+	return 0;
+}
