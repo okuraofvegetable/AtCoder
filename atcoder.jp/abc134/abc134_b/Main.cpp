5ae1e5d2ad3d237b8dc2dfb9929612cbe18d3165
okura <okuraofvegetable@gmail.com>
2019-08-14 17:58:01 +0900

    [AC] abc134 abc134_b

---
+++ atcoder.jp/abc134/abc134_b/Main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+int main(){
+	int N,D;
+	cin >> N >> D;
+	cout << (N-1)/(2*D+1)+1 << endl;
+	return 0;
+}
+
