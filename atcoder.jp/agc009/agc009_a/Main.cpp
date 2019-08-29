ad75cfbc66895b1bfb12aa7e6721e8ba89f78843
okura <okuraofvegetable@gmail.com>
2019-08-29 21:37:22 +0900

    [AC] agc009 agc009_a

---
+++ atcoder.jp/agc009/agc009_a/Main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef pair<P,int> T;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define LLINF 1000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+template<class T>
+void dump(vector<T> &vec){
+	for(int i=0;i<vec.size();i++){
+		cout << vec[i];
+		if(i+1<vec.size())cout << ' ';
+		else cout << endl;
+	}
+	return;
+}
+int N;
+ll A[100100],B[100100];
+int main(){
+	cin >> N;
+	for(int i=0;i<N;i++){
+		cin >> A[i] >> B[i];
+	}
+	ll acc = 0ll;
+	for(int i=N-1;i>=0;i--){
+		ll rem = (A[i]+acc)%B[i];
+		if(rem>0ll)rem = B[i]-rem;
+		acc += rem;
+	}
+	cout << acc << endl;	
+	return 0;
+}
