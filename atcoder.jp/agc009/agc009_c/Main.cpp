f8da33fa8cfe399988c26056939184162eca10c3
okura <okuraofvegetable@gmail.com>
2019-08-30 22:58:09 +0900

    [AC] agc009 agc009_c

---
+++ atcoder.jp/agc009/agc009_c/Main.cpp
@@ -0,0 +1,132 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define LLINF 1000000000000000002ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+template<class T>
+void dump(vector<T> &vec){
+	for(int i=0;i<vec.size();i++){
+		cout << vec[i];
+		if(i+1<vec.size())cout << ' ';
+		else cout << endl;
+	}
+	return;
+}
+#define MOD 1000000007ll // prime
+// How to devide :
+// ModInt a(6ll);
+// ModInt b(2ll);
+// a *= b.exp(MOD-2ll);  -> a/=b;  result: a = 3
+struct ModInt{
+	ll val;
+	ModInt():val(0ll){}
+	ModInt(ll v):val(((v%MOD)+MOD)%MOD){}
+	ModInt exp(ll y)const{
+		if(!y)return ModInt(1ll);
+		ModInt a = exp(y/2ll);
+		a *= a;
+		if(y&1)a*=(*this);
+		return a;
+	}
+	bool operator==(const ModInt& x)const{return val==x.val;}
+	inline bool operator!=(const ModInt& x)const{return !(*this==x);}
+	bool operator<(const ModInt& x)const{return val<x.val;}
+	bool operator>(const ModInt& x)const{return val>x.val;}
+	inline bool operator>=(const ModInt& x)const{return !(*this<x);}
+	inline bool operator<=(const ModInt& x)const{return !(*this>x);}
+	ModInt& operator+=(const ModInt& x){if((val+=x.val)>=MOD)val-=MOD;return *this;}
+	ModInt& operator-=(const ModInt& x){if((val+=MOD-x.val)>=MOD)val-=MOD;return *this;}
+	ModInt& operator*=(const ModInt& x){(val*=x.val)%=MOD;return *this;}
+	ModInt operator+(const ModInt& x)const{return ModInt(*this)+=x;}
+	ModInt operator-(const ModInt& x)const{return ModInt(*this)-=x;}
+	ModInt operator*(const ModInt& x)const{return ModInt(*this)*=x;}
+};
+istream& operator>>(istream&i,ModInt&x){i>>x.val;return i;}
+ostream& operator<<(ostream&o,const ModInt&x){o<<x.val;return o;}
+ModInt pow(ModInt a,ll x){
+	ModInt res = ModInt(1ll);
+	while(x){
+		if(x&1)res *= a;
+		x >>= 1;
+		a = a*a;
+	}
+	return res;
+}
+const int SIZE = 100100;
+ModInt inv[SIZE+10],fac[SIZE+10],facinv[SIZE+10];
+// notice: 0C0 = 1 
+ModInt nCr(int n,int r){
+	assert(!(n<r));
+	assert(!(n<0||r<0));
+	return fac[n]*facinv[r]*facinv[n-r];
+}
+void init(){
+	fac[0]=ModInt(1ll);
+	for(int i=1;i<=SIZE;i++)fac[i]=fac[i-1]*ModInt(i);
+	inv[1]=ModInt(1ll);
+	for(int i=2;i<=SIZE;i++)inv[i]=ModInt(0ll)-ModInt(MOD/i)*inv[MOD%i];
+	facinv[0]=ModInt(1ll);
+	for(int i=1;i<=SIZE;i++)facinv[i]=facinv[i-1]*inv[i];
+	return;
+}
+
+int N;
+ll A,B;
+ll S[100100];
+ModInt dp[100100];
+int main(){
+	cin >> N >> A >> B;
+	if(A>B)swap(A,B);
+	S[0] = -LLINF;
+	for(int i=0;i<N;i++){
+		cin >> S[i+1];
+	}
+	for(int i=3;i<=N;i++){
+		if(S[i]-S[i-2]<A){
+			cout << 0 << endl;
+			return 0;
+		}
+	}
+	dp[0] = ModInt(1ll);
+	int s = 0, t = 0;
+	ModInt sum;
+	for(int i=1;i<=N;i++){
+		while(t<i&&S[t]<=S[i]-B)sum += dp[t++];
+		ModInt tmp = sum;
+		// cout << sum << endl;
+		if(S[i]-S[i-1]<A){
+			while(s<i-1){
+				if(s<t)sum -= dp[s++];
+				else{
+					s++;
+					t++;
+				}
+			}
+		}
+		dp[i] = tmp;
+		// cout << i << " : " << dp[i] << endl;
+		// cout << s << ' ' << t << endl;
+	}
+	ModInt ans;
+	for(int i=s;i<=N;i++){
+		ans += dp[i];
+	}
+	cout << ans << endl;
+	return 0;
+}
