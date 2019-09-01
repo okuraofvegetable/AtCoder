a5cab689010608bfd573229f3ac5b0a2f84eaa61
okura <okuraofvegetable@gmail.com>
2019-09-01 20:15:14 +0900

    [AC] agc011 agc011_a

---
+++ atcoder.jp/agc011/agc011_a/Main.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define LLINF 1000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+template<class T>
+void dump(vector<T> &vec){
+	for(int i=0;i<vec.size();i++){
+		cout << vec[i];
+		if(i+1<vec.size())cout << ' ';
+		else cout << endl;
+	}
+	return;
+}
+int N,C,K;
+int T[100100];
+int main(){
+	cin >> N >> C >> K;
+	for(int i=0;i<N;i++)cin >> T[i];
+	sort(T,T+N);
+	int s = 0,t = 0;
+	int ans = 0;
+	while(s<N){
+		while(t<N&&T[t]<=T[s]+K){
+			t++;
+		}
+		ans++;
+		s = min(s+C,t);
+	}
+	cout << ans << endl;
+	return 0;
+}
