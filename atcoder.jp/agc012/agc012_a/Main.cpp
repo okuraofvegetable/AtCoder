33537509fd390c721062c35b7edc2ca5597531c8
okura <okuraofvegetable@gmail.com>
2019-09-02 12:41:06 +0900

    [AC] agc012 agc012_a

---
+++ atcoder.jp/agc012/agc012_a/Main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef pair<P,int> T;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define LLINF 1000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+template<class T>
+void dump(vector<T> &vec){
+	for(int i=0;i<vec.size();i++){
+		cout << vec[i];
+		if(i+1<vec.size())cout << ' ';
+		else cout << endl;
+	}
+	return;
+}
+int N;
+vector<ll> a;
+int main(){
+	cin >> N;
+	a.resize(3*N);
+	for(int i=0;i<a.size();i++){
+		cin >> a[i];
+	}
+	sort(all(a));
+	ll ans = 0ll;
+	for(int i=0;i<N;i++){
+		ans += a[a.size()-2*(i+1)];
+	}
+	cout << ans << endl;
+	return 0;
+}
