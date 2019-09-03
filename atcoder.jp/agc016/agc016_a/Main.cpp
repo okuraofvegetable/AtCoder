80e5f6fea19cd0881f75e41b7ca9700ac2bfaf7f
okura <okuraofvegetable@gmail.com>
2019-09-03 21:25:34 +0900

    [AC] agc016 agc016_a

---
+++ atcoder.jp/agc016/agc016_a/Main.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef pair<P,int> T;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define LLINF 1000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+template<class T>
+void dump(vector<T> &vec){
+	for(int i=0;i<vec.size();i++){
+		cout << vec[i];
+		if(i+1<vec.size())cout << ' ';
+		else cout << endl;
+	}
+	return;
+}
+template<class T>
+void input(vector<T>& vec,int n){
+	vec.resize(n);
+	for(int i=0;i<n;i++){
+		cin >> vec[i];
+	}
+}
+string s;
+int f(char a){
+	int ret = 0;
+	int tmp = 0;
+	for(int i=0;i<s.size();i++){
+		if(s[i]==a)tmp = 0;
+		else tmp++;
+		chmax(ret,tmp);
+	}
+	return ret;
+}
+int main(){
+	cin >> s;
+	int ans = 10000;
+	for(int i=0;i<26;i++){
+		chmin(ans,f((char)('a'+i)));
+	}
+	cout << ans << endl;
+	return 0;
+}
