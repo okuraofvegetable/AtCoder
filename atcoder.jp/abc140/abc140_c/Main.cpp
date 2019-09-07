8e809c2bb9f1ce2fcbd88a8cd32603ed3269f71a
okura <okuraofvegetable@gmail.com>
2019-09-07 22:35:48 +0900

    [AC] abc140 abc140_c

---
+++ atcoder.jp/abc140/abc140_c/Main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef pair<P,int> T;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define LLINF 1000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+template<class T>
+void dump(vector<T> &vec){
+	for(int i=0;i<vec.size();i++){
+		cout << vec[i];
+		if(i+1<vec.size())cout << ' ';
+		else cout << endl;
+	}
+	return;
+}
+int N;
+int B[1000];
+int ans = 0;
+int main(){
+	cin >> N;
+	for(int i=1;i<N;i++){
+		cin >> B[i];
+	}	
+	B[0] = B[N] = INF;
+	for(int i=0;i<N;i++){
+		ans += min(B[i],B[i+1]);
+	}
+	cout << ans << endl;
+	return 0;
+}
