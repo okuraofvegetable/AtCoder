ecee9fd5143e813960d478204c94b8d2f4ce3123
okura <okuraofvegetable@gmail.com>
2019-09-15 21:30:17 +0900

    [AC] abc141 abc141_a

---
+++ atcoder.jp/abc141/abc141_a/Main.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef pair<P,int> T;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define LLINF 1000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+#define dmp(x) cerr << __LINE__ << " " << #x << " " << x << endl;
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+template<class T>
+void dump(vector<T> &vec){
+	for(int i=0;i<vec.size();i++){
+		cout << vec[i];
+		if(i+1<vec.size())cout << ' ';
+		else cout << endl;
+	}
+	return;
+}
+template<class T>
+void input(vector<T>& vec,int n){
+	vec.resize(n);
+	for(int i=0;i<n;i++){
+		cin >> vec[i];
+	}
+}
+void fastio(){
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+	cout<<fixed<<setprecision(20);
+}
+string s[3];
+int main(){
+	s[0] = "Sunny";
+	s[1] = "Cloudy";
+	s[2] = "Rainy";
+	string t;
+	cin >> t;
+	for(int i=0;i<3;i++){
+		if(s[i]==t){
+			cout << s[(i+1)%3] << endl;
+		}
+	}
+	return 0;
+}
