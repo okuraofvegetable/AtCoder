ceb9fc97eaf4b55cc50358a64dfe33c7f5ca77a7
okura <okuraofvegetable@gmail.com>
2019-09-22 18:48:56 +0900

    [AC] agc038 agc038_a

---
+++ atcoder.jp/agc038/agc038_a/Main.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define LLINF 1000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+#define dmp(x) cerr << __LINE__ << " " << #x << " " << x << endl;
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+template<class T,class U>
+ostream& operator << (ostream& os,pair<T,U>& p){
+	os << p.fi << ',' << p.sec; return os;
+}
+template<class T,class U>
+istream& operator >> (istream& is,pair<T,U>& p){
+	is >> p.fi >> p.sec; return is;
+}
+template<class T>
+ostream& operator << (ostream &os,const vector<T> &vec){
+	for(int i=0;i<vec.size();i++){
+		os << vec[i];
+		if(i+1<vec.size())os << ' ';
+	}
+	return os;
+}
+template<class T>
+istream& operator >> (istream &is,vector<T>& vec){
+	for(int i=0;i<vec.size();i++)is >> vec[i];
+	return is;
+}
+void fastio(){
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+	cout<<fixed<<setprecision(20);
+}
+int H,W,A,B;
+int f[1010][1010];
+int main(){
+  cin >> H >> W >> A >> B;
+  for(int i=0;i<H;i++){
+    for(int j=0;j<W;j++){
+      int x = (i<B)?1:0;
+      int y = (j<A)?1:0;
+      if((x^y)==1)f[i][j] = 1;
+      cout << f[i][j];
+    }
+    cout << endl;
+  }
+	return 0;
+}
