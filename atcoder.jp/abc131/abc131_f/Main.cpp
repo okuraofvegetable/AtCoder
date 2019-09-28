aa54f37f03d9f384553fc7302d62d26ac07cdf08
okura <okuraofvegetable@gmail.com>
2019-09-28 12:47:35 +0900

    [AC] abc131 abc131_f

---
+++ atcoder.jp/abc131/abc131_f/Main.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000
+#define LLINF 1000000000000000ll
+#define sz(x) ((int)(x).size())
+#define fi first
+#define sec second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
+#define EQ(a,b) (abs((a)-(b))<eps)
+#define dmp(x) cerr << __LINE__ << " " << #x << " " << x << endl;
+template<class T> void chmin(T& a,const T& b){if(a>b)a=b;}
+template<class T> void chmax(T& a,const T& b){if(a<b)a=b;}
+template<class T>
+using MaxHeap = priority_queue<T>;
+template<class T>
+using MinHeap = priority_queue<T,vector<T>,greater<T> >;
+template<class T,class U>
+ostream& operator << (ostream& os,pair<T,U>& p){
+  os << p.fi << ',' << p.sec; return os;
+}
+template<class T,class U>
+istream& operator >> (istream& is,pair<T,U>& p){
+  is >> p.fi >> p.sec; return is;
+}
+template<class T>
+ostream& operator << (ostream &os,const vector<T> &vec){
+  for(int i=0;i<vec.size();i++){
+    os << vec[i];
+    if(i+1<vec.size())os << ' ';
+  }
+  return os;
+}
+template<class T>
+istream& operator >> (istream &is,vector<T>& vec){
+  for(int i=0;i<vec.size();i++)is >> vec[i];
+  return is;
+}
+void fastio(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout<<fixed<<setprecision(20);
+}
+const ll MOD = 1000000007ll; // if inv is needed, this shold be prime.
+struct ModInt{
+	ll val;
+	ModInt():val(0ll){}
+	ModInt(ll v):val(((v%MOD)+MOD)%MOD){}
+	ModInt exp(ll y)const{
+		if(!y)return ModInt(1ll);
+		ModInt a = exp(y/2ll);
+		a *= a;
+		if(y&1)a*=(*this);
+		return a;
+	}
+	bool operator==(const ModInt& x)const{return val==x.val;}
+	inline bool operator!=(const ModInt& x)const{return !(*this==x);}
+	bool operator<(const ModInt& x)const{return val<x.val;}
+	bool operator>(const ModInt& x)const{return val>x.val;}
+	inline bool operator>=(const ModInt& x)const{return !(*this<x);}
+	inline bool operator<=(const ModInt& x)const{return !(*this>x);}
+	ModInt& operator+=(const ModInt& x){if((val+=x.val)>=MOD)val-=MOD;return *this;}
+	ModInt& operator-=(const ModInt& x){if((val+=MOD-x.val)>=MOD)val-=MOD;return *this;}
+	ModInt& operator*=(const ModInt& x){(val*=x.val)%=MOD;return *this;}
+	ModInt operator+(const ModInt& x)const{return ModInt(*this)+=x;}
+	ModInt operator-(const ModInt& x)const{return ModInt(*this)-=x;}
+	ModInt operator*(const ModInt& x)const{return ModInt(*this)*=x;}
+  istream& operator>>(istream&i){i>>(this->val);return i;}
+  ostream& operator<<(ostream&o)const{o<<(this->val);return o;}
+  ModInt pow(ModInt a,ll x) const {
+    ModInt res = ModInt(1ll);
+    while(x){
+      if(x&1)res *= a;
+      x >>= 1;
+      a = a*a;
+    }
+    return res;
+  }
+};
+vector<ModInt> inv,fac,facinv;
+// notice: 0C0 = 1 
+ModInt nCr(int n,int r){
+	assert(!(n<r));
+	assert(!(n<0||r<0));
+	return fac[n]*facinv[r]*facinv[n-r];
+}
+void init(int SIZE){
+  fac.resize(SIZE+10);
+  inv.resize(SIZE+10);
+  facinv.resize(SIZE+10);
+	fac[0]=ModInt(1ll);
+	for(int i=1;i<=SIZE;i++)fac[i]=fac[i-1]*ModInt(i);
+	inv[1]=ModInt(1ll);
+	for(int i=2;i<=SIZE;i++)inv[i]=ModInt(0ll)-ModInt(MOD/i)*inv[MOD%i];
+	facinv[0]=ModInt(1ll);
+	for(int i=1;i<=SIZE;i++)facinv[i]=facinv[i-1]*inv[i];
+	return;
+}
+int N;
+vector<int> g[200100];
+bool used[200100];
+pair<ll,ll> dfs(int v){
+  used[v] = true;
+  pair<ll,ll> ret = make_pair(0ll,0ll);
+  for(int i=0;i<g[v].size();i++){
+    int u = g[v][i];
+    if(used[u])continue;
+    pair<ll,ll> a = dfs(u);
+    ret.fi += a.fi;
+    ret.sec += a.sec;
+  }
+  swap(ret.fi,ret.sec);
+  ret.fi++;
+  return ret;
+}
+int main(){
+  cin >> N;
+  for(int i=0;i<N;i++){
+    int x,y;
+    cin >> x >> y;
+    g[x].pb(y+100000);
+    g[y+100000].pb(x);
+  }
+  ll ans = 0ll;
+  for(int i=0;i<=200000;i++){
+    if(!used[i]){
+      auto a = dfs(i);
+      ans += a.fi*a.sec;
+    }
+  }
+  cout << ans-N << endl;
+  return 0;
+}
