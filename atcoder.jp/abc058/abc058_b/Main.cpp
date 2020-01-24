2f36bf1b7998eb9b0e1f0a59e24a692f66fc57d9
okura <okuraofvegetable@gmail.com>
2020-01-24 12:55:46 +0900

    [AC] abc058 abc058_b

---
+++ atcoder.jp/abc058/abc058_b/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+  string s,t;
+  cin >> s >> t;
+  for(int i=0;i<s.size();i++){
+    cout << s[i];
+    if(i<t.size())cout << t[i];
+  }
+  cout << endl;
+  return 0;
+}
