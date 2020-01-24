836a3b709d974ff2325f705dd4a3fdd06e44f65b
okura <okuraofvegetable@gmail.com>
2020-01-24 13:05:53 +0900

    [AC] abc112 abc112_a

---
+++ atcoder.jp/abc112/abc112_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int t;
+  cin >> t;
+  if(t==1)cout << "Hello World" << endl;
+  else{
+    int a,b;
+    cin >> a >> b;
+    cout << a+b << endl;
+  }
+  return 0;
+}
