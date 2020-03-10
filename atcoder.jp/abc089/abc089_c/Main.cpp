79d9ac68018bb6c8172bb55202885f9de4330bc1
okura <okuraofvegetable@gmail.com>
2020-03-10 09:21:59 +0900

    [AC] abc089 abc089_c

---
+++ atcoder.jp/abc089/abc089_c/Main.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+// #define int long long
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000               // 2e9
+#define LLINF 2000000000000000000ll  // 2e18 (llmax:9e18)
+#define fi first
+#define sec second
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+#define dmp(x) cerr << #x << ": " << x << endl;
+
+template <class T>
+void chmin(T &a, const T &b) {
+  if (a > b) a = b;
+}
+template <class T>
+void chmax(T &a, const T &b) {
+  if (a < b) a = b;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  os << p.fi << ',' << p.sec;
+  return os;
+}
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  is >> p.fi >> p.sec;
+  return is;
+}
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) {
+    os << vec[i];
+    if (i + 1 < vec.size()) os << ' ';
+  }
+  return os;
+}
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+#define endl "\n"
+
+template <class T>
+struct RangeSum {
+  vector<T> vec;
+  RangeSum() {}
+  RangeSum(vector<T> elems) : vec(elems) {
+    for (int i = 1; i < vec.size(); i++) { vec[i] += vec[i - 1]; }
+  }
+  T sum(int l, int r) {
+    if (l > r) return T(0);
+    if (l == 0)
+      return vec[r];
+    else
+      return vec[r] - vec[l - 1];
+  }
+};
+
+void solve() {
+  ll ans = 0ll;
+  map<char, ll> m;
+  int N;
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    string s;
+    cin >> s;
+    m[s[0]]++;
+  }
+  ans += (m['M'] * m['A'] * m['R']);
+  ans += (m['M'] * m['A'] * m['C']);
+  ans += (m['M'] * m['A'] * m['H']);
+  ans += (m['M'] * m['R'] * m['C']);
+  ans += (m['M'] * m['R'] * m['H']);
+  ans += (m['M'] * m['C'] * m['H']);
+  ans += (m['A'] * m['R'] * m['C']);
+  ans += (m['A'] * m['R'] * m['H']);
+  ans += (m['A'] * m['C'] * m['H']);
+  ans += (m['R'] * m['C'] * m['H']);
+  cout << ans << endl;
+  return;
+}
+
+signed main() {
+  fastio();
+  solve();
+  //   int t;
+  //   cin >> t;
+  //   while (t--) solve();
+  return 0;
+}
