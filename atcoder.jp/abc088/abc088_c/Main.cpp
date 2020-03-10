70ecf606489c0cc3a4072a80cc7a402eb47323bf
okura <okuraofvegetable@gmail.com>
2020-03-10 09:42:24 +0900

    [AC] abc088 abc088_c

---
+++ atcoder.jp/abc088/abc088_c/Main.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+// #define int long long
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000               // 2e9
+#define LLINF 2000000000000000000ll  // 2e18 (llmax:9e18)
+#define fi first
+#define sec second
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+#define dmp(x) cerr << #x << ": " << x << endl;
+
+template <class T>
+void chmin(T &a, const T &b) {
+  if (a > b) a = b;
+}
+template <class T>
+void chmax(T &a, const T &b) {
+  if (a < b) a = b;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  os << p.fi << ',' << p.sec;
+  return os;
+}
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  is >> p.fi >> p.sec;
+  return is;
+}
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) {
+    os << vec[i];
+    if (i + 1 < vec.size()) os << ' ';
+  }
+  return os;
+}
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+#define endl "\n"
+
+template <class T>
+struct RangeSum {
+  vector<T> vec;
+  RangeSum() {}
+  RangeSum(vector<T> elems) : vec(elems) {
+    for (int i = 1; i < vec.size(); i++) { vec[i] += vec[i - 1]; }
+  }
+  T sum(int l, int r) {
+    if (l > r) return T(0);
+    if (l == 0)
+      return vec[r];
+    else
+      return vec[r] - vec[l - 1];
+  }
+};
+
+void solve() {
+  vector<vector<int>> f = vect(3, vect(3, 0));
+  cin >> f;
+  for (int i = -200; i <= 200; i++) {
+    for (int j = -200; j <= 200; j++) {
+      vector<int> a(3), b(3);
+      a[0] = i, b[0] = j;
+      for (int k = 1; k < 3; k++) {
+        a[k] = f[k][0] - f[0][0] + a[0];
+        b[k] = f[0][k] - f[0][0] + b[0];
+      }
+      bool judge = true;
+      for (int p = 0; p < 3; p++) {
+        for (int q = 0; q < 3; q++) {
+          if (a[p] + b[q] != f[p][q]) judge = false;
+        }
+      }
+      if (judge) {
+        cout << "Yes" << endl;
+        return;
+      }
+    }
+  }
+  cout << "No" << endl;
+  return;
+}
+
+signed main() {
+  fastio();
+  solve();
+  //   int t;
+  //   cin >> t;
+  //   while (t--) solve();
+  return 0;
+}
