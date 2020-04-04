b19ed1d2c0631a4a729cb499f39ab684049d63a0
okura <okuraofvegetable@gmail.com>
2020-04-05 00:36:01 +0900

    [AC] s8pc-3 s8pc_3_a

---
+++ atcoder.jp/s8pc-3/s8pc_3_a/Main.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+// #define int long long
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000               // 2e9
+#define LLINF 2000000000000000000ll  // 2e18 (llmax:9e18)
+#define fi first
+#define sec second
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+#define dmp(x) cerr << #x << ": " << x << endl;
+
+template <class T>
+void chmin(T &a, const T &b) {
+  if (a > b) a = b;
+}
+template <class T>
+void chmax(T &a, const T &b) {
+  if (a < b) a = b;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  os << p.fi << ',' << p.sec;
+  return os;
+}
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  is >> p.fi >> p.sec;
+  return is;
+}
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) {
+    os << vec[i];
+    if (i + 1 < vec.size()) os << ' ';
+  }
+  return os;
+}
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+#define endl "\n"
+
+void solve() {
+  ll n;
+  ll k;
+  cin >> n >> k;
+  if (n <= 2) {
+    cout << 0 << endl;
+    return;
+  }
+  ll a = (k + 5ll) * 5ll % 11ll;
+  dmp(a);
+  ll quo = (n - 2) / 11;
+  ll rem = (n - 2) % 11;
+  //   dmp(quo);
+  //   dmp(rem);
+  ll sum = 0ll, ans = 0ll;
+  for (ll i = 1; i <= 11; i++) {
+    for (ll j = 1; j <= 5; j++) {
+      if ((7ll * i + j - 7ll) % 11 == a) sum++;
+    }
+    if (i == rem) ans += sum;
+  }
+  ans += quo * sum;
+  cout << ans << endl;
+  return;
+}
+
+signed main() {
+  fastio();
+  solve();
+  // int t; cin >> t; while(t--)solve();
+  return 0;
+}
