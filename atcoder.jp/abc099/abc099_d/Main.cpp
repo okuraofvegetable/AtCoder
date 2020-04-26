9b1c7605e691bf47a1d75b1483e6b92aa6bcc1c8
okura <okuraofvegetable@gmail.com>
2020-04-26 12:18:48 +0900

    [AC] abc099 abc099_d

---
+++ atcoder.jp/abc099/abc099_d/Main.cpp
@@ -0,0 +1,134 @@
+// #pragma GCC optimize("unroll-loops", "omit-frame-pointer", "inline")
+// #pragma GCC option("arch=native", "tune=native", "no-zero-upper")
+// #pragma GCC
+// target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("tree-vectorize","openmp","predictive-commoning")
+// #pragma GCC option("D_GLIBCXX_PARALLEL","openmp")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+// #define int long long
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000               // 2e9
+#define LLINF 2000000000000000000ll  // 2e18 (llmax:9e18)
+#define fi first
+#define sec second
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+#define dmp(x) cerr << #x << ": " << x << endl;
+
+template <class T>
+void chmin(T &a, const T &b) {
+  if (a > b) a = b;
+}
+template <class T>
+void chmax(T &a, const T &b) {
+  if (a < b) a = b;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  os << p.fi << ',' << p.sec;
+  return os;
+}
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  is >> p.fi >> p.sec;
+  return is;
+}
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) {
+    os << vec[i];
+    if (i + 1 < vec.size()) os << ' ';
+  }
+  return os;
+}
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+#define endl "\n"
+
+template <class T>
+vector<T> divisor(T n) {
+  vector<T> ret;
+  for (T i = 1; i * i <= n; i++) {
+    if (n % i == 0) {
+      ret.push_back(i);
+      if (i * i != n) ret.push_back(n / i);
+    }
+  }
+  sort(begin(ret), end(ret));
+  return ret;
+}
+
+void solve() {
+  int N, C;
+  cin >> N >> C;
+  auto D = vect(C, vect(C, 0));
+  cin >> D;
+  auto c = vect(N, vect(N, 0));
+  cin >> c;
+  auto cnt = vect(3, vect(C, 0));
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) { cnt[(i + j) % 3][c[i][j] - 1]++; }
+  }
+  int ans = INF;
+  for (int a = 0; a < C; a++) {
+    for (int b = 0; b < C; b++) {
+      if (a == b) continue;
+      for (int c = 0; c < C; c++) {
+        if (a == c || b == c) continue;
+        int diff = 0;
+        for (int d = 0; d < C; d++) {
+          diff += cnt[0][d] * D[d][a];
+          diff += cnt[1][d] * D[d][b];
+          diff += cnt[2][d] * D[d][c];
+        }
+        chmin(ans, diff);
+      }
+    }
+  }
+  cout << ans << endl;
+  return;
+}
+
+signed main() {
+  fastio();
+  solve();
+  //   int t;
+  //   cin >> t;
+  //   while (t--) solve();
+
+  // int t; cin >> t;
+  // for(int i=1;i<=t;i++){
+  //   cout << "Case #" << i << ": ";
+  //   solve();
+  // }
+  return 0;
+}
