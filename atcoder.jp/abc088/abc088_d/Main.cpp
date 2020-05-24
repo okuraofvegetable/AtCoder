fb689667b4a715cdf5a8ab2c4d88eeccd7492e94
okura <okuraofvegetable@gmail.com>
2020-05-24 14:58:19 +0900

    [AC] abc088 abc088_d

---
+++ atcoder.jp/abc088/abc088_d/Main.cpp
@@ -0,0 +1,135 @@
+// #pragma GCC optimize("unroll-loops", "omit-frame-pointer", "inline")
+// #pragma GCC option("arch=native", "tune=native", "no-zero-upper")
+// #pragma GCC
+// target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("tree-vectorize","openmp","predictive-commoning")
+// #pragma GCC option("D_GLIBCXX_PARALLEL","openmp")
+
+// #pragma GCC optimize("O3")
+// #pragma GCC target("avx2")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+// #define int long long
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000               // 2e9
+#define LLINF 2000000000000000000ll  // 2e18 (llmax:9e18)
+#define fi first
+#define sec second
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+#define dmp(x) cerr << #x << ": " << x << endl;
+
+template <class T>
+void chmin(T &a, const T &b) {
+  if (a > b) a = b;
+}
+template <class T>
+void chmax(T &a, const T &b) {
+  if (a < b) a = b;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  os << p.fi << ',' << p.sec;
+  return os;
+}
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  is >> p.fi >> p.sec;
+  return is;
+}
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) {
+    os << vec[i];
+    if (i + 1 < vec.size()) os << ' ';
+  }
+  return os;
+}
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+#define endl "\n"
+
+void solve() {
+  int H, W;
+  cin >> H >> W;
+  vector<string> s(H);
+  cin >> s;
+  int cnt = 0;
+  for (int i = 0; i < H; i++) {
+    for (int j = 0; j < W; j++) {
+      if (s[i][j] == '#') cnt++;
+    }
+  }
+  vector<int> dx = {-1, 0, 1, 0};
+  vector<int> dy = {0, 1, 0, -1};
+  auto dist = vect(H, vect(W, INF));
+  queue<P> q;
+  q.push(P(0, 0));
+  dist[0][0] = 0;
+  while (!q.empty()) {
+    P a = q.front();
+    q.pop();
+    int x = a.first;
+    int y = a.second;
+    for (int i = 0; i < 4; i++) {
+      int nx = x + dx[i];
+      int ny = y + dy[i];
+      if (nx < 0 || nx >= H || ny < 0 || ny >= W) continue;
+      if (s[nx][ny] == '#') continue;
+      if (dist[nx][ny] == INF) {
+        dist[nx][ny] = dist[x][y] + 1;
+        q.push(P(nx, ny));
+      }
+    }
+  }
+  // for (auto &col : dist) cout << col << endl;
+  if (dist[H - 1][W - 1] == INF)
+    cout << -1 << endl;
+  else
+    cout << H * W - dist[H - 1][W - 1] - 1 - cnt << endl;
+  return;
+}
+
+signed main() {
+  fastio();
+  solve();
+  //   int t;
+  //   cin >> t;
+  //   while (t--) solve();
+
+  // int t; cin >> t;
+  // for(int i=1;i<=t;i++){
+  //   cout << "Case #" << i << ": ";
+  //   solve();
+  // }
+  return 0;
+}
