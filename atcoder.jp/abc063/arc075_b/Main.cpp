1ae33f2dc248ad4e83988b0628514717fff43ba6
okura <okuraofvegetable@gmail.com>
2020-05-25 23:56:24 +0900

    [AC] abc063 arc075_b

---
+++ atcoder.jp/abc063/arc075_b/Main.cpp
@@ -0,0 +1,220 @@
+// #pragma GCC optimize("unroll-loops", "omit-frame-pointer", "inline")
+// #pragma GCC option("arch=native", "tune=native", "no-zero-upper")
+// #pragma GCC
+// target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("tree-vectorize","openmp","predictive-commoning")
+// #pragma GCC option("D_GLIBCXX_PARALLEL","openmp")
+
+// #pragma GCC optimize("O3")
+// #pragma GCC target("avx2")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+// #define int long long
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define INF 2000000000               // 2e9
+#define LLINF 2000000000000000000ll  // 2e18 (llmax:9e18)
+#define fi first
+#define sec second
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+#define dmp(x) cerr << #x << ": " << x << endl;
+
+template <class T>
+void chmin(T &a, const T &b) {
+  if (a > b) a = b;
+}
+template <class T>
+void chmax(T &a, const T &b) {
+  if (a < b) a = b;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  os << p.fi << ',' << p.sec;
+  return os;
+}
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  is >> p.fi >> p.sec;
+  return is;
+}
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) {
+    os << vec[i];
+    if (i + 1 < vec.size()) os << ' ';
+  }
+  return os;
+}
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+#define endl "\n"
+
+template <class Cost = int>
+struct edge {
+  int from, to;
+  Cost cost;
+  edge() {}
+  edge(int f, int t, Cost cost) : from(f), to(t), cost(cost) {}
+};
+
+template <class Cost = int>
+struct Tree {
+  int V;
+  int LOG_V;
+  int root;
+  vector<int> depth;
+  vector<vector<int>> dpar;
+  vector<int> par;
+  vector<int> subtree_size;
+  vector<Cost> dist;  // distance from root node
+  vector<vector<edge<Cost>>> G;
+  Tree() {}
+  Tree(int V, int r = 0) : V(V), root(r) {
+    G.resize(V);
+    depth.resize(V);
+    par.resize(V);
+    dist.resize(V);
+    subtree_size.resize(V);
+  }
+  void add_edge(int u, int v, Cost c = Cost(0)) {
+    G[u].pb(edge<Cost>(u, v, c));
+    G[v].pb(edge<Cost>(v, u, c));
+  }
+  int dfs(int v, int p, int d, Cost c) {
+    depth[v] = d;
+    par[v] = p;
+    subtree_size[v] = 1;
+    dist[v] = c;
+    bool is_centroid = true;
+    for (auto e : G[v]) {
+      int u = e.to;
+      if (u == p) continue;
+      subtree_size[v] += dfs(u, v, d + 1, c + e.cost);
+    }
+    return subtree_size[v];
+  }
+  void dfs() { dfs(root, -1, 0, Cost(0)); }
+
+  void construct_dpar() {
+    LOG_V = 1;
+    while ((1 << LOG_V) <= V) LOG_V++;
+    dpar.resize(LOG_V);
+    for (int i = 0; i < LOG_V; i++) dpar[i].resize(V, -1);
+    for (int i = 0; i < V; i++) dpar[0][i] = par[i];
+    for (int i = 1; i < LOG_V; i++) {
+      for (int j = 0; j < V; j++) {
+        if (dpar[i - 1][j] == -1)
+          dpar[i][j] = -1;
+        else
+          dpar[i][j] = dpar[i - 1][dpar[i - 1][j]];
+      }
+    }
+    return;
+  }
+  int kth_parent(int v, int k) {
+    int res = v;
+    for (int i = 0; i < LOG_V; i++) {
+      if (k & (1 << i)) res = dpar[i][res];
+      if (res == -1) return -1;
+    }
+    return res;
+  }
+  int lca(int u, int v) {
+    if (depth[u] > depth[v]) swap(u, v);
+    v = kth_parent(v, depth[v] - depth[u]);
+    if (u == v) return v;
+    for (int i = LOG_V - 1; i >= 0; i--) {
+      if (dpar[i][u] != dpar[i][v]) {
+        u = dpar[i][u];
+        v = dpar[i][v];
+      }
+    }
+    return par[v];
+  }
+  Cost distance(int u, int v) {
+    int l = lca(u, v);
+    return dist[u] + dist[v] - dist[l] - dist[l];
+  }
+  int size() { return V; }
+  void dump_tree() {
+    for (int i = 0; i < V; i++) {
+      printf("id: %d, par %d, depth %d\n", i, par[i], depth[i]);
+    }
+    return;
+  }
+};
+
+void solve() {
+  int N;
+  ll A, B;
+  cin >> N >> A >> B;
+  vector<ll> h(N);
+  cin >> h;
+  ll l = 0, r = LLINF;
+  auto check = [&](ll k) {
+    auto tmp = h;
+    for (int i = 0; i < N; i++) {
+      if (k > tmp[i] / B)
+        tmp[i] = 0;
+      else
+        tmp[i] = max(0ll, tmp[i] - B * k);
+    }
+    ll req = 0ll;
+    for (int i = 0; i < N; i++) { req += (tmp[i] + A - B - 1) / (A - B); }
+    // dmp(req);
+    return req <= k;
+  };
+  while (r - l > 1) {
+    ll mid = (l + r) / 2;
+    if (check(mid))
+      r = mid;
+    else
+      l = mid;
+  }
+  cout << r << endl;
+  return;
+}
+
+signed main() {
+  fastio();
+  solve();
+  //   int t;
+  //   cin >> t;
+  //   while (t--) solve();
+
+  // int t; cin >> t;
+  // for(int i=1;i<=t;i++){
+  //   cout << "Case #" << i << ": ";
+  //   solve();
+  // }
+  return 0;
+}
