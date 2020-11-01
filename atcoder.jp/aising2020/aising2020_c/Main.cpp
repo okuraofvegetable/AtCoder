012d764be9de8ea74db80dd807fd436660b05916
okura <okuraofvegetable@gmail.com>
2020-11-01 16:54:13 +0900

    [AC] aising2020 aising2020_c

---
+++ atcoder.jp/aising2020/aising2020_c/Main.cpp
@@ -0,0 +1,223 @@
+// #pragma GCC optimize("unroll-loops", "omit-frame-pointer", "inline")
+// #pragma GCC option("arch=native", "tune=native", "no-zero-upper")
+// #pragma GCC
+// target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("tree-vectorize","openmp","predictive-commoning")
+// #pragma GCC option("D_GLIBCXX_PARALLEL","openmp")
+
+// #pragma GCC optimize("O3")
+// #pragma GCC target("avx2")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+#define eps 1e-9
+#define INF 2000000000              // 2e9
+#define LLINF 2000000000000000000ll // 2e18 (llmax:9e18)
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+
+#ifndef LOCAL
+#define dmp(...) ;
+#else
+#define dmp(...)                                                               \
+  cerr << "[ " << #__VA_ARGS__ << " ] : " << dump_str(__VA_ARGS__) << endl
+#endif
+
+// ---------------- Utility ------------------
+
+template <class T>
+bool chmin(T &a, const T &b) {
+  if (a <= b) return false;
+  a = b;
+  return true;
+}
+template <class T>
+bool chmax(T &a, const T &b) {
+  if (a >= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+// ----------------- Input -------------------
+
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  is >> p.first >> p.second;
+  return is;
+}
+
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+
+// ----------------- Output ------------------
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  os << p.first << ',' << p.second;
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  for (const T &e : v) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const deque<T> &d) {
+  for (const T &e : d) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  os << "}";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const multiset<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  os << "}";
+  return os;
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &m) {
+  os << "{ ";
+  for (const auto &[key, val] : m) os << "( " << key << " -> " << val << " ) ";
+  os << "}";
+  return os;
+}
+
+void dump_str_rec(ostringstream &) {}
+
+template <class Head, class... Tail>
+void dump_str_rec(ostringstream &oss, Head &&head, Tail &&... tail) {
+  oss << ", " << head;
+  dump_str_rec(oss, forward<Tail>(tail)...);
+}
+
+template <class T, class... U>
+string dump_str(T &&arg, U &&... args) {
+  ostringstream oss;
+  oss << arg;
+  dump_str_rec(oss, forward<T>(args)...);
+  return oss.str();
+}
+
+// --------------- Fast I/O ------------------
+
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+// ------------ End of template --------------
+
+#define endl "\n"
+
+#include <atcoder/modint>
+
+// using mint = atcoder::static_modint<998244353>;
+
+template <class D>
+struct SegmentTree {
+  using DMerger = function<D(D, D)>;
+  int length;
+  vector<D> seg;
+  const D d_unit;
+  const DMerger dm;
+
+  SegmentTree() {}
+  SegmentTree(int n, const DMerger dm, const D &d_unit)
+      : dm(dm), d_unit(d_unit) {
+    length = 1;
+    while (length < n) length <<= 1;
+    seg.assign(2 * length, d_unit);
+  }
+  SegmentTree(vector<D> vec, const DMerger dm, const D &d_unit)
+      : dm(dm), d_unit(d_unit) {
+    length = 1;
+    while (length < vec.size()) length <<= 1;
+    seg.assign(2 * length, d_unit);
+    for (int i = 0; i < vec.size(); i++) seg[length - 1 + i] = vec[i];
+    for (int i = length - 2; i >= 0; i--)
+      seg[i] = dm(seg[i * 2 + 1], seg[i * 2 + 2]);
+  }
+
+  void update(int k, D x) {
+    k += length - 1;
+    seg[k] = x;
+    while (k) {
+      k = (k - 1) / 2;
+      seg[k] = dm(seg[k * 2 + 1], seg[k * 2 + 2]);
+    }
+  }
+
+  D query(int a, int b, int k, int l, int r) {
+    if (r <= a || b <= l) {
+      return d_unit;
+    } else if (a <= l && r <= b) {
+      return seg[k];
+    } else {
+      D lch = query(a, b, k * 2 + 1, l, (l + r) / 2);
+      D rch = query(a, b, k * 2 + 2, (l + r) / 2, r);
+      return dm(lch, rch);
+    }
+  }
+  D query(int a, int b) { return query(a, b, 0, 0, length); }
+  D get_point(int x) { return seg[length - 1 + x]; }
+};
+
+void solve() {
+  int N;
+  cin >> N;
+  vector<int> f(N + 1);
+  for (int x = 1; x <= 100; x++) {
+    for (int y = 1; y <= 100; y++) {
+      for (int z = 1; z <= 100; z++) {
+        int r = x * x + y * y + z * z + x * y + y * z + z * x;
+        if (r <= N) f[r]++;
+      }
+    }
+  }
+  for (int i = 1; i <= N; i++) cout << f[i] << endl;
+  return;
+}
+
+int main() {
+  fastio();
+  solve();
+  // int t; cin >> t; while(t--)solve();
+
+  // int t; cin >> t;
+  // for(int i=1;i<=t;i++){
+  //   cout << "Case #" << i << ": ";
+  //   solve();
+  // }
+  return 0;
+}
