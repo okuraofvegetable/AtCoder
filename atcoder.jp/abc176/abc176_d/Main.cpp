5b04f72085425a61c483451678f82e6b8616b9ab
okura <okuraofvegetable@gmail.com>
2020-11-01 11:15:28 +0900

    [AC] abc176 abc176_d

---
+++ atcoder.jp/abc176/abc176_d/Main.cpp
@@ -0,0 +1,202 @@
+// #pragma GCC optimize("unroll-loops", "omit-frame-pointer", "inline")
+// #pragma GCC option("arch=native", "tune=native", "no-zero-upper")
+// #pragma GCC
+// target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("tree-vectorize","openmp","predictive-commoning")
+// #pragma GCC option("D_GLIBCXX_PARALLEL","openmp")
+
+// #pragma GCC optimize("O3")
+// #pragma GCC target("avx2")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+#define eps 1e-9
+#define INF 2000000000              // 2e9
+#define LLINF 2000000000000000000ll // 2e18 (llmax:9e18)
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+
+#ifndef LOCAL
+#define dmp(...) ;
+#else
+#define dmp(...)                                                               \
+  cerr << "[ " << #__VA_ARGS__ << " ] : " << dump_str(__VA_ARGS__) << endl
+#endif
+
+// ---------------- Utility ------------------
+
+template <class T>
+bool chmin(T &a, const T &b) {
+  if (a <= b) return false;
+  a = b;
+  return true;
+}
+template <class T>
+bool chmax(T &a, const T &b) {
+  if (a >= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+// ----------------- Input -------------------
+
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  is >> p.first >> p.second;
+  return is;
+}
+
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+
+// ----------------- Output ------------------
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  os << p.first << ',' << p.second;
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  for (const T &e : v) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const deque<T> &d) {
+  for (const T &e : d) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  os << "}";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const multiset<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  os << "}";
+  return os;
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &m) {
+  os << "{ ";
+  for (const auto &[key, val] : m) os << "( " << key << " -> " << val << " ) ";
+  os << "}";
+  return os;
+}
+
+void dump_str_rec(ostringstream &) {}
+
+template <class Head, class... Tail>
+void dump_str_rec(ostringstream &oss, Head &&head, Tail &&... tail) {
+  oss << ", " << head;
+  dump_str_rec(oss, forward<Tail>(tail)...);
+}
+
+template <class T, class... U>
+string dump_str(T &&arg, U &&... args) {
+  ostringstream oss;
+  oss << arg;
+  dump_str_rec(oss, forward<T>(args)...);
+  return oss.str();
+}
+
+// --------------- Fast I/O ------------------
+
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+// ------------ End of template --------------
+
+#define endl "\n"
+
+#include <atcoder/modint>
+
+using mint = atcoder::static_modint<998244353>;
+
+void solve() {
+  int H, W;
+  cin >> H >> W;
+  int sx, sy, gx, gy;
+  cin >> sx >> sy;
+  cin >> gx >> gy;
+  sx--;
+  sy--;
+  gx--;
+  gy--;
+  vector<string> S(H);
+  cin >> S;
+  deque<P> q;
+  auto dist = vect(H, vect(W, INF));
+  q.push_back(P(sx, sy));
+  dist[sx][sy] = 0;
+  while (!q.empty()) {
+    auto [x, y] = q.front();
+    q.pop_front();
+    for (int i = -2; i <= 2; i++) {
+      for (int j = -2; j <= 2; j++) {
+        if (i == 0 && j == 0) continue;
+        int nx = x + i;
+        int ny = y + j;
+        if (nx < 0 || nx >= H || ny < 0 || ny >= W) continue;
+        if (S[nx][ny] == '#') continue;
+        int cost = (abs(i) + abs(j) == 1) ? 0 : 1;
+        if (chmin(dist[nx][ny], dist[x][y] + cost)) {
+          if (cost == 0)
+            q.push_front(P(nx, ny));
+          else
+            q.push_back(P(nx, ny));
+        }
+      }
+    }
+  }
+  for (auto &r : dist) dmp(r);
+  if (dist[gx][gy] == INF)
+    cout << -1 << endl;
+  else
+    cout << dist[gx][gy] << endl;
+  return;
+}
+
+int main() {
+  fastio();
+  solve();
+  // int t; cin >> t; while(t--)solve();
+
+  // int t; cin >> t;
+  // for(int i=1;i<=t;i++){
+  //   cout << "Case #" << i << ": ";
+  //   solve();
+  // }
+  return 0;
+}
