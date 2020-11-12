a958081bb3f8701d8f305a258115023cd918d863
okura <okuraofvegetable@gmail.com>
2020-11-12 13:41:41 +0900

    [AC] arc047 arc047_a

---
+++ atcoder.jp/arc047/arc047_a/Main.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+#define eps 1e-9
+#define INF 2000000000              // 2e9
+#define LLINF 2000000000000000000ll // 2e18 (llmax:9e18)
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+
+#ifndef LOCAL
+#define dmp(...) ;
+#else
+#define dmp(...)                                                               \
+  cerr << "[ " << #__VA_ARGS__ << " ] : " << dump_str(__VA_ARGS__) << endl
+#endif
+
+// ---------------- Utility ------------------
+
+template <class T>
+bool chmin(T &a, const T &b) {
+  if (a <= b) return false;
+  a = b;
+  return true;
+}
+template <class T>
+bool chmax(T &a, const T &b) {
+  if (a >= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+// ----------------- Input -------------------
+
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  return is >> p.first >> p.second;
+}
+
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+
+// ----------------- Output ------------------
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  return os << p.first << ',' << p.second;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  for (const T &e : v) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const deque<T> &d) {
+  for (const T &e : d) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  return os << "}";
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &m) {
+  os << "{ ";
+  for (const auto &[key, val] : m) os << "( " << key << " -> " << val << " ) ";
+  return os << "}";
+}
+
+void dump_str_rec(ostringstream &) {}
+
+template <class Head, class... Tail>
+void dump_str_rec(ostringstream &oss, Head &&head, Tail &&... tail) {
+  oss << ", " << head;
+  dump_str_rec(oss, forward<Tail>(tail)...);
+}
+
+template <class T, class... U>
+string dump_str(T &&arg, U &&... args) {
+  ostringstream oss;
+  oss << arg;
+  dump_str_rec(oss, forward<U>(args)...);
+  return oss.str();
+}
+
+// --------------- Fast I/O ------------------
+
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+// ------------ End of template --------------
+
+#define endl "\n"
+
+void solve() {
+  int n, l;
+  cin >> n >> l;
+  string s;
+  cin >> s;
+  int ans = 0;
+  int cur = 1;
+  for (int i = 0; i < s.size(); i++) {
+    if (s[i] == '+')
+      cur++;
+    else
+      cur--;
+    if (cur > l) {
+      ans++;
+      cur = 1;
+    }
+  }
+  cout << ans << endl;
+  return;
+}
+
+int main() {
+  fastio();
+  solve();
+  // while (solve()) {}
+  // int t; cin >> t; while(t--)solve();
+
+  // int t; cin >> t;
+  // for(int i=1;i<=t;i++){
+  //   cout << "Case #" << i << ": ";
+  //   solve();
+  // }
+  return 0;
+}
