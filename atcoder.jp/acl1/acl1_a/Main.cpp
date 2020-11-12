514a34ea305680fa7a39f1ffcf616d6d516af4d5
okura <okuraofvegetable@gmail.com>
2020-11-12 14:42:11 +0900

    [AC] acl1 acl1_a

---
+++ atcoder.jp/acl1/acl1_a/Main.cpp
@@ -0,0 +1,189 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+#define eps 1e-9
+#define INF 2000000000              // 2e9
+#define LLINF 2000000000000000000ll // 2e18 (llmax:9e18)
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+
+#ifndef LOCAL
+#define dmp(...) ;
+#else
+#define dmp(...)                                                               \
+  cerr << "[ " << #__VA_ARGS__ << " ] : " << dump_str(__VA_ARGS__) << endl
+#endif
+
+// ---------------- Utility ------------------
+
+template <class T>
+bool chmin(T &a, const T &b) {
+  if (a <= b) return false;
+  a = b;
+  return true;
+}
+template <class T>
+bool chmax(T &a, const T &b) {
+  if (a >= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+// ----------------- Input -------------------
+
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  return is >> p.first >> p.second;
+}
+
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+
+// ----------------- Output ------------------
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  return os << p.first << ',' << p.second;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  for (const T &e : v) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const deque<T> &d) {
+  for (const T &e : d) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  return os << "}";
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &m) {
+  os << "{ ";
+  for (const auto &[key, val] : m) os << "( " << key << " -> " << val << " ) ";
+  return os << "}";
+}
+
+void dump_str_rec(ostringstream &) {}
+
+template <class Head, class... Tail>
+void dump_str_rec(ostringstream &oss, Head &&head, Tail &&... tail) {
+  oss << ", " << head;
+  dump_str_rec(oss, forward<Tail>(tail)...);
+}
+
+template <class T, class... U>
+string dump_str(T &&arg, U &&... args) {
+  ostringstream oss;
+  oss << arg;
+  dump_str_rec(oss, forward<U>(args)...);
+  return oss.str();
+}
+
+// --------------- Fast I/O ------------------
+
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+// ------------ End of template --------------
+
+#define endl "\n"
+
+struct UnionFind {
+  vector<int> par, rank, sz;
+  UnionFind(int n) {
+    par.assign(n, 0);
+    rank.assign(n, 0);
+    sz.assign(n, 1);
+    for (int i = 0; i < n; i++) par[i] = i;
+  }
+  int find(const int x) { return (par[x] == x) ? x : (par[x] = find(par[x])); }
+  void unite(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if (x == y) return;
+    if (rank[x] < rank[y]) {
+      par[x] = y;
+      sz[y] += sz[x];
+    } else {
+      par[y] = x;
+      sz[x] += sz[y];
+      if (rank[x] == rank[y]) rank[x]++;
+    }
+  }
+  bool same(const int x, const int y) { return find(x) == find(y); }
+};
+
+void solve() {
+  int N;
+  cin >> N;
+  vector<int> x(N), y(N);
+  vector<int> rev(N);
+  for (int i = 0; i < N; i++) {
+    cin >> x[i] >> y[i];
+    x[i]--;
+    y[i]--;
+    rev[x[i]] = i;
+  }
+  set<P> s;
+  UnionFind uf(N);
+  for (int i = N - 1; i >= 0; i--) {
+    int id = rev[i];
+    auto it = s.upper_bound(P(y[id], -1));
+    if (it == s.end())
+      s.emplace(y[id], id);
+    else {
+      P mx(-1, -1);
+      for (auto iter = it; iter != s.end();) {
+        uf.unite(id, iter->second);
+        mx = *iter;
+        iter = s.erase(iter);
+      }
+      s.insert(mx);
+    }
+  }
+  for (int i = 0; i < N; i++) cout << uf.sz[uf.find(i)] << endl;
+  return;
+}
+
+int main() {
+  fastio();
+  solve();
+  // while (solve()) {}
+  // int t; cin >> t; while(t--)solve();
+
+  // int t; cin >> t;
+  // for(int i=1;i<=t;i++){
+  //   cout << "Case #" << i << ": ";
+  //   solve();
+  // }
+  return 0;
+}
