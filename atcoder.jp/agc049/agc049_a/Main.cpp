90ff6aee39e237f9c429b72e410e3caeb10bec5f
okura <okuraofvegetable@gmail.com>
2020-11-14 21:53:49 +0900

    [AC] agc049 agc049_a

---
+++ atcoder.jp/agc049/agc049_a/Main.cpp
@@ -0,0 +1,265 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+#define eps 1e-9
+#define INF 2000000000              // 2e9
+#define LLINF 2000000000000000000ll // 2e18 (llmax:9e18)
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+
+#ifndef LOCAL
+#define dmp(...) ;
+#else
+#define dmp(...)                                                               \
+  cerr << "[ " << #__VA_ARGS__ << " ] : " << dump_str(__VA_ARGS__) << endl
+#endif
+
+// ---------------- Utility ------------------
+
+template <class T>
+bool chmin(T &a, const T &b) {
+  if (a <= b) return false;
+  a = b;
+  return true;
+}
+template <class T>
+bool chmax(T &a, const T &b) {
+  if (a >= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+// ----------------- Input -------------------
+
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  return is >> p.first >> p.second;
+}
+
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+
+// ----------------- Output ------------------
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  return os << p.first << ',' << p.second;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  for (const T &e : v) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const deque<T> &d) {
+  for (const T &e : d) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  return os << "}";
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &m) {
+  os << "{ ";
+  for (const auto &[key, val] : m) os << "( " << key << " -> " << val << " ) ";
+  return os << "}";
+}
+
+void dump_str_rec(ostringstream &) {}
+
+template <class Head, class... Tail>
+void dump_str_rec(ostringstream &oss, Head &&head, Tail &&... tail) {
+  oss << ", " << head;
+  dump_str_rec(oss, forward<Tail>(tail)...);
+}
+
+template <class T, class... U>
+string dump_str(T &&arg, U &&... args) {
+  ostringstream oss;
+  oss << arg;
+  dump_str_rec(oss, forward<U>(args)...);
+  return oss.str();
+}
+
+// --------------- Fast I/O ------------------
+
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+// ------------ End of template --------------
+
+#define endl "\n"
+
+template <class Cost = int>
+struct edge {
+  int from, to;
+  Cost cost;
+  edge() {}
+  edge(int from, int to, Cost cost) : from(from), to(to), cost(cost) {}
+
+  // for debug
+  friend ostream &operator<<(ostream &os, const edge &e) {
+    os << e.from << " -> " << e.to << " : " << e.cost << ", ";
+    return os;
+  }
+};
+
+template <class Cost = int>
+struct Graph {
+  int V;
+  vector<vector<edge<Cost>>> G;
+  Graph() {}
+  Graph(int V) : V(V) { G.resize(V); }
+  void add_edge(int a, int b, Cost cost = Cost(0)) {
+    G[a].push_back(edge<Cost>(a, b, cost));
+  }
+  void add_edge_undirected(int a, int b, Cost cost = Cost(0)) {
+    G[a].push_back(edge<Cost>(a, b, cost));
+    G[b].push_back(edge<Cost>(b, a, cost));
+  }
+  size_t size() const { return G.size(); }
+  const vector<edge<Cost>> &operator[](int id) const { return G[id]; }
+
+  // for debug
+  friend ostream &operator<<(ostream &os, const Graph g) {
+    for (int i = 0; i < g.size(); i++) {
+      os << g[i];
+      if (i + 1 < g.size()) os << endl;
+    }
+    return os;
+  }
+};
+
+// verified https://judge.yosupo.jp/submission/3440
+template <class Graph>
+struct SCC {
+  Graph G, rG; // Graph and reverse Graph
+  vector<bool> used;
+  vector<int> vs;
+  vector<int> cmp_id;      // component id
+  vector<vector<int>> cmp; // components
+  explicit SCC(const Graph G) : G(G) {
+    rG = Graph(G.size());
+    used.resize(G.size(), false);
+    cmp_id.resize(G.size(), -1);
+    for (int i = 0; i < G.size(); i++) {
+      for (auto e : G[i]) { rG.add_edge(e.to, e.from, e.cost); }
+    }
+    for (int i = 0; i < G.size(); i++) {
+      if (!used[i]) dfs(i);
+    }
+    used.assign(G.size(), false);
+    reverse(vs.begin(), vs.end());
+    int id = 0;
+    for (int i = 0; i < vs.size(); i++) {
+      if (!used[vs[i]]) {
+        cmp.push_back(vector<int>());
+        rdfs(vs[i], id++);
+      }
+    }
+  }
+  void dfs(int s) {
+    used[s] = true;
+    for (const auto &e : G[s]) {
+      if (!used[e.to]) dfs(e.to);
+    }
+    vs.push_back(s);
+  }
+  void rdfs(int s, int k) {
+    used[s] = true;
+    cmp_id[s] = k;
+    cmp[k].push_back(s);
+    for (const auto &e : rG[s]) {
+      if (!used[e.to]) rdfs(e.to, k);
+    }
+  }
+};
+
+void solve() {
+  int N;
+  cin >> N;
+  vector<string> S(N);
+  cin >> S;
+  Graph g(N);
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (S[i][j] == '1') g.add_edge(i, j);
+    }
+  }
+  SCC<decltype(g)> scc(g);
+  vector<vector<int>> cg(scc.cmp.size());
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (S[i][j] == '1' && scc.cmp_id[i] != scc.cmp_id[j])
+        cg[scc.cmp_id[j]].push_back(scc.cmp_id[i]);
+    }
+  }
+  for (int i = 0; i < cg.size(); i++) {
+    sort(all(cg[i]));
+    cg[i].erase(unique(all(cg[i])), cg[i].end());
+  }
+
+  vector<int> num(cg.size(), 0);
+  for (int i = 0; i < cg.size(); i++) {
+    vector<bool> used(cg.size(), false);
+    auto dfs = [&](auto &&f, int v) -> void {
+      used[v] = true;
+      num[i] += scc.cmp[v].size();
+      for (int u : cg[v])
+        if (!used[u]) f(f, u);
+      return;
+    };
+    dfs(dfs, i);
+  }
+  double ans = 0;
+  for (int i = 0; i < N; i++) {
+    // dmp(i, scc.cmp_id[i]);
+    ans += (1.0 / num[scc.cmp_id[i]]);
+  }
+  dmp(num);
+  cout << ans << endl;
+  return;
+}
+
+int main() {
+  fastio();
+  solve();
+  // while (solve()) {}
+  // int t;
+  // cin >> t;
+  // while (t--) solve();
+
+  // int t; cin >> t;
+  // for(int i=1;i<=t;i++){
+  //   cout << "Case #" << i << ": ";
+  //   solve();
+  // }
+  return 0;
+}
