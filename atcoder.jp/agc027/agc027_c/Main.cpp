8782d046f5dd7a08e815bfa4096347ee2d0612f1
okura <okuraofvegetable@gmail.com>
2020-12-09 01:18:04 +0900

    [AC] agc027 agc027_c

---
+++ atcoder.jp/agc027/agc027_c/Main.cpp
@@ -0,0 +1,192 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+#define eps 1e-9
+#define INF 2000000000              // 2e9
+#define LLINF 2000000000000000000ll // 2e18 (llmax:9e18)
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+
+#ifndef LOCAL
+#define dmp(...) ;
+#else
+#define dmp(...)                                                               \
+  cerr << "[ " << #__VA_ARGS__ << " ] : " << dump_str(__VA_ARGS__) << endl
+#endif
+
+// ---------------- Utility ------------------
+
+template <class T>
+bool chmin(T &a, const T &b) {
+  if (a <= b) return false;
+  a = b;
+  return true;
+}
+template <class T>
+bool chmax(T &a, const T &b) {
+  if (a >= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+// ----------------- Input -------------------
+
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  return is >> p.first >> p.second;
+}
+
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+
+// ----------------- Output ------------------
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  return os << p.first << ',' << p.second;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  for (const T &e : v) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const deque<T> &d) {
+  for (const T &e : d) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  return os << "}";
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &m) {
+  os << "{ ";
+  for (const auto &[key, val] : m) os << "( " << key << " -> " << val << " ) ";
+  return os << "}";
+}
+
+template <class TupleTy, size_t... I>
+void dump_tuple(ostream &os, const TupleTy t, std::index_sequence<I...>) {
+  (..., (os << (I == 0 ? "" : ",") << std::get<I>(t)));
+}
+
+template <class... Args>
+ostream &operator<<(ostream &os, const tuple<Args...> &t) {
+  os << "(";
+  dump_tuple(os, t, std::make_index_sequence<sizeof...(Args)>());
+  return os << ")";
+}
+
+void dump_str_rec(ostringstream &) {}
+
+template <class Head, class... Tail>
+void dump_str_rec(ostringstream &oss, Head &&head, Tail &&... tail) {
+  oss << ", " << head;
+  dump_str_rec(oss, forward<Tail>(tail)...);
+}
+
+template <class T, class... U>
+string dump_str(T &&arg, U &&... args) {
+  ostringstream oss;
+  oss << arg;
+  dump_str_rec(oss, forward<U>(args)...);
+  return oss.str();
+}
+
+// --------------- Fast I/O ------------------
+
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+// ------------ End of template --------------
+
+#define endl "\n"
+
+void solve() {
+  int N, M;
+  cin >> N >> M;
+  string s;
+  cin >> s;
+  vector<vector<int>> g(N);
+  auto adj = vect(N, vect(2, 0));
+  for (int i = 0; i < M; i++) {
+    int a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    g[a].push_back(b);
+    g[b].push_back(a);
+    adj[b][s[a] - 'A']++;
+    adj[a][s[b] - 'A']++;
+  }
+  vector<bool> used(N, false);
+  queue<int> q;
+  int cnt = 0;
+  for (int i = 0; i < N; i++) {
+    if (!adj[i][0] || !adj[i][1]) {
+      used[i] = true;
+      cnt++;
+      q.push(i);
+    }
+  }
+  while (!q.empty()) {
+    int v = q.front();
+    q.pop();
+    int c = s[v] - 'A';
+    for (int u : g[v]) {
+      adj[u][c]--;
+      if (!used[u] && adj[u][c] == 0) {
+        used[u] = true;
+        cnt++;
+        q.push(u);
+      }
+    }
+  }
+  if (cnt == N)
+    cout << "No" << endl;
+  else
+    cout << "Yes" << endl;
+  return;
+}
+
+int main() {
+  fastio();
+  solve();
+  // int t;
+  // cin >> t;
+  // while (t--) solve();
+
+  // int t; cin >> t;
+  // for(int i=1;i<=t;i++){
+  //   cout << "Case #" << i << ": ";
+  //   solve();
+  // }
+  return 0;
+}
