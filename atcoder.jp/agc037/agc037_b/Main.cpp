8c65cc3041b760e58203803ca7a03ad934f986ec
okura <okuraofvegetable@gmail.com>
2020-12-14 14:52:26 +0900

    [AC] agc037 agc037_b

---
+++ atcoder.jp/agc037/agc037_b/Main.cpp
@@ -0,0 +1,286 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+#define eps 1e-9
+#define INF 2000000000              // 2e9
+#define LLINF 2000000000000000000ll // 2e18 (llmax:9e18)
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+
+#ifndef LOCAL
+#define dmp(...) ;
+#else
+#define dmp(...)                                                               \
+  cerr << "[ " << #__VA_ARGS__ << " ] : " << dump_str(__VA_ARGS__) << endl
+#endif
+
+// ---------------- Utility ------------------
+
+template <class T>
+bool chmin(T &a, const T &b) {
+  if (a <= b) return false;
+  a = b;
+  return true;
+}
+template <class T>
+bool chmax(T &a, const T &b) {
+  if (a >= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+// ----------------- Input -------------------
+
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  return is >> p.first >> p.second;
+}
+
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+
+// ----------------- Output ------------------
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  return os << p.first << ',' << p.second;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  for (const T &e : v) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const deque<T> &d) {
+  for (const T &e : d) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  return os << "}";
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &m) {
+  os << "{ ";
+  for (const auto &[key, val] : m) os << "( " << key << " -> " << val << " ) ";
+  return os << "}";
+}
+
+template <class TupleTy, size_t... I>
+void dump_tuple(ostream &os, const TupleTy t, std::index_sequence<I...>) {
+  (..., (os << (I == 0 ? "" : ",") << std::get<I>(t)));
+}
+
+template <class... Args>
+ostream &operator<<(ostream &os, const tuple<Args...> &t) {
+  os << "(";
+  dump_tuple(os, t, std::make_index_sequence<sizeof...(Args)>());
+  return os << ")";
+}
+
+void dump_str_rec(ostringstream &) {}
+
+template <class Head, class... Tail>
+void dump_str_rec(ostringstream &oss, Head &&head, Tail &&... tail) {
+  oss << ", " << head;
+  dump_str_rec(oss, forward<Tail>(tail)...);
+}
+
+template <class T, class... U>
+string dump_str(T &&arg, U &&... args) {
+  ostringstream oss;
+  oss << arg;
+  dump_str_rec(oss, forward<U>(args)...);
+  return oss.str();
+}
+
+// --------------- Fast I/O ------------------
+
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+// ------------ End of template --------------
+
+#define endl "\n"
+
+template <int MOD> // if inv is needed, this shold be prime.
+struct ModInt {
+  ll val;
+  ModInt() : val(0ll) {}
+  ModInt(const ll &v) : val(((v % MOD) + MOD) % MOD) {}
+  bool operator==(const ModInt &x) const { return val == x.val; }
+  bool operator!=(const ModInt &x) const { return !(*this == x); }
+  bool operator<(const ModInt &x) const { return val < x.val; }
+  bool operator>(const ModInt &x) const { return val > x.val; }
+  bool operator>=(const ModInt &x) const { return !(*this < x); }
+  bool operator<=(const ModInt &x) const { return !(*this > x); }
+  ModInt operator-() const { return ModInt(MOD - val); }
+  ModInt inv() const { return this->pow(MOD - 2); }
+  ModInt &operator+=(const ModInt &x) {
+    if ((val += x.val) >= MOD) val -= MOD;
+    return *this;
+  }
+  ModInt &operator-=(const ModInt &x) {
+    if ((val += MOD - x.val) >= MOD) val -= MOD;
+    return *this;
+  }
+  ModInt &operator*=(const ModInt &x) {
+    (val *= x.val) %= MOD;
+    return *this;
+  }
+  ModInt &operator/=(const ModInt &x) { return *this *= x.inv(); };
+  ModInt operator+(const ModInt &x) const { return ModInt(*this) += x; }
+  ModInt operator-(const ModInt &x) const { return ModInt(*this) -= x; }
+  ModInt operator*(const ModInt &x) const { return ModInt(*this) *= x; }
+  ModInt operator/(const ModInt &x) const { return ModInt(*this) /= x; }
+  friend istream &operator>>(istream &i, ModInt &x) {
+    ll v;
+    i >> v;
+    x = v;
+    return i;
+  }
+  friend ostream &operator<<(ostream &o, const ModInt &x) {
+    o << x.val;
+    return o;
+  }
+  ModInt pow(ll x) const {
+    auto res = ModInt(1ll);
+    auto b = *this;
+    while (x) {
+      if (x & 1) res *= b;
+      x >>= 1;
+      b *= b;
+    }
+    return res;
+  }
+};
+
+template <int MOD>
+ModInt<MOD> pow(ModInt<MOD> a, ll x) {
+  ModInt<MOD> res = ModInt<MOD>(1ll);
+  while (x) {
+    if (x & 1) res *= a;
+    x >>= 1;
+    a *= a;
+  }
+  return res;
+}
+
+// constexpr int MOD = 1e9 + 7;
+constexpr int MOD = 998244353;
+using mint = ModInt<MOD>;
+
+vector<mint> inv, fac, facinv;
+// notice: 0C0 = 1
+mint nCr(int n, int r) {
+  assert(!(n < r));
+  assert(!(n < 0 || r < 0));
+  return fac[n] * facinv[r] * facinv[n - r];
+}
+
+void init(int SIZE) {
+  fac.resize(SIZE + 1);
+  inv.resize(SIZE + 1);
+  facinv.resize(SIZE + 1);
+  fac[0] = inv[1] = facinv[0] = mint(1ll);
+  for (int i = 1; i <= SIZE; i++) fac[i] = fac[i - 1] * mint(i);
+  for (int i = 2; i <= SIZE; i++)
+    inv[i] = mint(0ll) - mint(MOD / i) * inv[MOD % i];
+  for (int i = 1; i <= SIZE; i++) facinv[i] = facinv[i - 1] * inv[i];
+  return;
+}
+
+void solve() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  mint ans(1);
+  // map<char, int> m;
+  // vector<int> a(n + 1, INF);
+  // vector<int> c(n + 1, -INF);
+  // for (int i = 0; i < s.size(); i++) {
+  //   m[s[i]]++;
+  //   chmin(a[m[s[i]]], i);
+  //   chmax(c[m[s[i]]], i);
+  // }
+  // vector<int> rui(s.size(), 0);
+  // vector<bool> isB(s.size(), true);
+  // for (int i = 1; i <= n; i++) {
+  //   rui[a[i]]++;
+  //   rui[c[i]]--;
+  //   isB[a[i]] = isB[c[i]] = false;
+  // }
+  // dmp(a, c);
+  // dmp(rui);
+  // dmp(isB);
+  // for (int i = 0; i < s.size(); i++) {
+  //   if (i > 0) rui[i] += rui[i - 1];
+  //   if (isB[i]) { ans *= mint(rui[i]); }
+  // }
+  // ans *= fac[n];
+  // cout << ans << endl;
+
+  vector<int> cnt(8);
+  cnt[0] = n;
+  map<char, int> m;
+  for (int i = 0; i < 3; i++) m["RGB"[i]] = i;
+  for (int i = 0; i < s.size(); i++) {
+    int v = m[s[i]];
+    int mask = 1 << v;
+    int best = -1;
+    for (int i = 0; i < 8; i++) {
+      if ((i & mask) != 0) continue;
+      if (cnt[i] == 0) continue;
+      if (best == -1 ||
+          __builtin_popcount(best | mask) < __builtin_popcount(i | mask))
+        best = i;
+    }
+    assert(best != -1);
+    ans *= cnt[best];
+    cnt[best]--;
+    cnt[best | mask]++;
+  }
+  cout << ans << endl;
+  return;
+}
+
+int main() {
+  fastio();
+  init(100100);
+  solve();
+  // int t; cin >> t; while(t--)solve();
+
+  // int t; cin >> t;
+  // for(int i=1;i<=t;i++){
+  //   cout << "Case #" << i << ": ";
+  //   solve();
+  // }
+  return 0;
+}
