e4b05099f2d14c3caed09b21b2bdf87d69fe4757
okura <okuraofvegetable@gmail.com>
2020-12-31 23:50:17 +0900

    [AC] arc093 arc093_c

---
+++ atcoder.jp/arc093/arc093_c/Main.cpp
@@ -0,0 +1,350 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+#define eps 1e-9
+#define INF 2000000000              // 2e9
+#define LLINF 2000000000000000000ll // 2e18 (llmax:9e18)
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+
+#define rep(i, x) for (int i = 0; i < (int)(x); i++)
+#define drep(i, x) for (int i = ((int)(x)-1); i >= 0; i--)
+
+#define popcount __builtin_popcount
+#define next __next
+#define prev __prev
+
+#ifndef LOCAL
+#define dmp(...) ;
+#else
+#define dmp(...)                                                               \
+  cerr << "[ " << #__VA_ARGS__ << " ] : " << dump_str(__VA_ARGS__) << endl
+#endif
+
+// ---------------- Utility ------------------
+
+template <class T>
+bool chmin(T &a, const T &b) {
+  if (a <= b) return false;
+  a = b;
+  return true;
+}
+template <class T>
+bool chmax(T &a, const T &b) {
+  if (a >= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+// ----------------- Input -------------------
+
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  return is >> p.first >> p.second;
+}
+
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+
+// ----------------- Output ------------------
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  return os << p.first << ',' << p.second;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  for (const T &e : v) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const deque<T> &d) {
+  for (const T &e : d) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  return os << "}";
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &m) {
+  os << "{ ";
+  for (const auto &[key, val] : m) os << "( " << key << " -> " << val << " ) ";
+  return os << "}";
+}
+
+template <class TupleTy, size_t... I>
+void dump_tuple(ostream &os, const TupleTy t, std::index_sequence<I...>) {
+  (..., (os << (I == 0 ? "" : ",") << std::get<I>(t)));
+}
+
+template <class... Args>
+ostream &operator<<(ostream &os, const tuple<Args...> &t) {
+  os << "(";
+  dump_tuple(os, t, std::make_index_sequence<sizeof...(Args)>());
+  return os << ")";
+}
+
+void dump_str_rec(ostringstream &) {}
+
+template <class Head, class... Tail>
+void dump_str_rec(ostringstream &oss, Head &&head, Tail &&... tail) {
+  oss << ", " << head;
+  dump_str_rec(oss, forward<Tail>(tail)...);
+}
+
+template <class T, class... U>
+string dump_str(T &&arg, U &&... args) {
+  ostringstream oss;
+  oss << arg;
+  dump_str_rec(oss, forward<U>(args)...);
+  return oss.str();
+}
+
+// --------------- Fast I/O ------------------
+
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+// ------------ End of template --------------
+
+#define endl "\n"
+
+struct UnionFind {
+  vector<int> par, rank, sz;
+  UnionFind(int n) {
+    par.assign(n, 0);
+    rank.assign(n, 0);
+    sz.assign(n, 1);
+    for (int i = 0; i < n; i++) par[i] = i;
+  }
+  int find(const int x) { return (par[x] == x) ? x : (par[x] = find(par[x])); }
+  void unite(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if (x == y) return;
+    if (rank[x] < rank[y]) {
+      par[x] = y;
+      sz[y] += sz[x];
+    } else {
+      par[y] = x;
+      sz[x] += sz[y];
+      if (rank[x] == rank[y]) rank[x]++;
+    }
+  }
+  bool same(const int x, const int y) { return find(x) == find(y); }
+  int size(const int x) { return sz[find(x)]; }
+  vector<vector<int>> components() {
+    int n = par.size();
+    vector<vector<int>> cs;
+    vector<int> idx(n, -1);
+    for (int i = 0; i < n; i++) {
+      if (find(i) != i) continue;
+      idx[i] = cs.size();
+      cs.emplace_back();
+    }
+    for (int i = 0; i < n; i++) {
+      int id = idx[find(i)];
+      cs[id].push_back(i);
+    }
+    return cs;
+  }
+};
+
+template <int MOD> // if inv is needed, this shold be prime.
+struct ModInt {
+  ll val;
+  ModInt() : val(0ll) {}
+  ModInt(const ll &v) : val(((v % MOD) + MOD) % MOD) {}
+  bool operator==(const ModInt &x) const { return val == x.val; }
+  bool operator!=(const ModInt &x) const { return !(*this == x); }
+  bool operator<(const ModInt &x) const { return val < x.val; }
+  bool operator>(const ModInt &x) const { return val > x.val; }
+  bool operator>=(const ModInt &x) const { return !(*this < x); }
+  bool operator<=(const ModInt &x) const { return !(*this > x); }
+  ModInt operator-() const { return ModInt(MOD - val); }
+  ModInt inv() const { return this->pow(MOD - 2); }
+  ModInt &operator+=(const ModInt &x) {
+    if ((val += x.val) >= MOD) val -= MOD;
+    return *this;
+  }
+  ModInt &operator-=(const ModInt &x) {
+    if ((val += MOD - x.val) >= MOD) val -= MOD;
+    return *this;
+  }
+  ModInt &operator*=(const ModInt &x) {
+    (val *= x.val) %= MOD;
+    return *this;
+  }
+  ModInt &operator/=(const ModInt &x) { return *this *= x.inv(); };
+  ModInt operator+(const ModInt &x) const { return ModInt(*this) += x; }
+  ModInt operator-(const ModInt &x) const { return ModInt(*this) -= x; }
+  ModInt operator*(const ModInt &x) const { return ModInt(*this) *= x; }
+  ModInt operator/(const ModInt &x) const { return ModInt(*this) /= x; }
+  friend istream &operator>>(istream &i, ModInt &x) {
+    ll v;
+    i >> v;
+    x = v;
+    return i;
+  }
+  friend ostream &operator<<(ostream &o, const ModInt &x) {
+    o << x.val;
+    return o;
+  }
+  ModInt pow(ll x) const {
+    auto res = ModInt(1ll);
+    auto b = *this;
+    while (x) {
+      if (x & 1) res *= b;
+      x >>= 1;
+      b *= b;
+    }
+    return res;
+  }
+};
+
+template <int MOD>
+ModInt<MOD> pow(ModInt<MOD> a, ll x) {
+  ModInt<MOD> res = ModInt<MOD>(1ll);
+  while (x) {
+    if (x & 1) res *= a;
+    x >>= 1;
+    a *= a;
+  }
+  return res;
+}
+
+constexpr int MOD = 1e9 + 7;
+// constexpr int MOD = 998244353;
+using mint = ModInt<MOD>;
+
+vector<mint> inv, fac, facinv;
+// notice: 0C0 = 1
+mint nCr(int n, int r) {
+  assert(!(n < r));
+  assert(!(n < 0 || r < 0));
+  return fac[n] * facinv[r] * facinv[n - r];
+}
+
+void init(int SIZE) {
+  fac.resize(SIZE + 1);
+  inv.resize(SIZE + 1);
+  facinv.resize(SIZE + 1);
+  fac[0] = inv[1] = facinv[0] = mint(1ll);
+  for (int i = 1; i <= SIZE; i++) fac[i] = fac[i - 1] * mint(i);
+  for (int i = 2; i <= SIZE; i++)
+    inv[i] = mint(0ll) - mint(MOD / i) * inv[MOD % i];
+  for (int i = 1; i <= SIZE; i++) facinv[i] = facinv[i - 1] * inv[i];
+  return;
+}
+
+void solve() {
+  int N, M;
+  cin >> N >> M;
+  ll X;
+  cin >> X;
+  using edge = tuple<ll, int, int>;
+  vector<edge> es;
+  rep(i, M) {
+    int u, v, w;
+    cin >> u >> v >> w;
+    u--;
+    v--;
+    es.emplace_back(w, u, v);
+  }
+  sort(all(es));
+  vector<bool> mst(M, false);
+  vector<ll> diff(M, -1);
+  ll T = 0;
+  UnionFind uf(N);
+  vector<vector<pair<int, ll>>> g(N);
+  for (int i = 0; i < es.size(); i++) {
+    auto [w, u, v] = es[i];
+    if (!uf.same(u, v)) {
+      uf.unite(u, v);
+      g[u].emplace_back(v, w);
+      g[v].emplace_back(u, w);
+      T += w;
+      mst[i] = true;
+    }
+  }
+
+  vector<ll> mx(N);
+  function<void(int, int, ll)> dfs = [&](int v, int p, ll d) {
+    mx[v] = d;
+    for (auto [u, w] : g[v]) {
+      if (u == p) continue;
+      dfs(u, v, max(d, w));
+    }
+  };
+
+  for (int i = 0; i < M; i++) {
+    if (mst[i]) continue;
+    auto [w, u, v] = es[i];
+    mx.assign(N, -LLINF);
+    dfs(u, -1, -LLINF);
+    diff[i] = w - mx[v];
+    assert(diff[i] >= 0ll);
+  }
+  dmp(diff);
+
+  mint ans(0);
+  if (T == X) { ans += (mint(2).pow(N - 1) - 2) * (mint(2).pow(M - (N - 1))); }
+  dmp(ans);
+  if (X >= T) {
+    ll k = X - T;
+    mint B(1);
+    int cnt = 0;
+    for (int i = 0; i < M; i++) {
+      if (mst[i]) continue;
+      if (diff[i] > k)
+        B *= 2;
+      else if (diff[i] == k)
+        cnt++;
+    }
+    B *= (mint(2).pow(cnt) - mint(1));
+    B *= 2;
+    ans += B;
+  }
+  cout << ans << endl;
+  return;
+}
+
+int main() {
+  fastio();
+  solve();
+  // int t; cin >> t; while(t--)solve();
+
+  // int t; cin >> t;
+  // for(int i=1;i<=t;i++){
+  //   cout << "Case #" << i << ": ";
+  //   solve();
+  // }
+  return 0;
+}
