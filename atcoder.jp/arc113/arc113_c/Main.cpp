15453e55af19235b3c414ba861624f7a389b2b4e
okura <okuraofvegetable@gmail.com>
2021-02-21 21:33:37 +0900

    [AC] arc113 arc113_c

---
+++ atcoder.jp/arc113/arc113_c/Main.cpp
@@ -0,0 +1,188 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+#define eps 1e-9
+#define INF 2000000000              // 2e9
+#define LLINF 2000000000000000000ll // 2e18 (llmax:9e18)
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+
+#define rep(i, x) for (int i = 0; i < (int)(x); i++)
+#define drep(i, x) for (int i = ((int)(x)-1); i >= 0; i--)
+
+#define popcount __builtin_popcount
+#define next __next
+#define prev __prev
+
+#ifndef LOCAL
+#define dmp(...) ;
+#else
+#define dmp(...)                                                               \
+  cerr << "[ " << #__VA_ARGS__ << " ] : " << dump_str(__VA_ARGS__) << endl
+#endif
+
+// ---------------- Utility ------------------
+
+template <class T>
+bool chmin(T &a, const T &b) {
+  if (a <= b) return false;
+  a = b;
+  return true;
+}
+template <class T>
+bool chmax(T &a, const T &b) {
+  if (a >= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+// ----------------- Input -------------------
+
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  return is >> p.first >> p.second;
+}
+
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+
+// ----------------- Output ------------------
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  return os << p.first << ',' << p.second;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  for (const T &e : v) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const deque<T> &d) {
+  for (const T &e : d) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  return os << "}";
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &m) {
+  os << "{ ";
+  for (const auto &[key, val] : m) os << "( " << key << " -> " << val << " ) ";
+  return os << "}";
+}
+
+template <class TupleTy, size_t... I>
+void dump_tuple(ostream &os, const TupleTy t, std::index_sequence<I...>) {
+  (..., (os << (I == 0 ? "" : ",") << std::get<I>(t)));
+}
+
+template <class... Args>
+ostream &operator<<(ostream &os, const tuple<Args...> &t) {
+  os << "(";
+  dump_tuple(os, t, std::make_index_sequence<sizeof...(Args)>());
+  return os << ")";
+}
+
+void dump_str_rec(ostringstream &) {}
+
+template <class Head, class... Tail>
+void dump_str_rec(ostringstream &oss, Head &&head, Tail &&... tail) {
+  oss << ", " << head;
+  dump_str_rec(oss, forward<Tail>(tail)...);
+}
+
+template <class T, class... U>
+string dump_str(T &&arg, U &&... args) {
+  ostringstream oss;
+  oss << arg;
+  dump_str_rec(oss, forward<U>(args)...);
+  return oss.str();
+}
+
+// --------------- Fast I/O ------------------
+
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+// ------------ End of template --------------
+
+#define endl "\n"
+
+void solve() {
+  string S;
+  cin >> S;
+  int N = S.size();
+
+  auto rui = vect(26, vect(N + 1, 0));
+  for (int i = 0; i < N; i++) {
+    rui[S[i] - 'a'][i + 1]++;
+    for (int j = 0; j < 26; j++) rui[j][i + 1] += rui[j][i];
+  }
+
+  auto count = [&](int l, int r, char c) {
+    int ch = c - 'a';
+    return rui[ch][r] - rui[ch][l];
+  };
+
+  vector<int> ds;
+  for (int i = 0; i + 1 < S.size(); i++) {
+    if (S[i] == S[i + 1]) ds.push_back(i);
+  }
+  ll ans = 0ll;
+  for (int i = (int)ds.size() - 1; i >= 0; i--) {
+    int d = ds[i];
+    if (i + 1 == ds.size()) {
+      ans += N - (d + 2) - count(d + 2, N, S[d]);
+    } else if (S[ds[i + 1]] == S[d]) {
+      if (ds[i + 1] > d + 2) {
+        ans += ds[i + 1] - (d + 2) - count(d + 2, ds[i + 1], S[d]);
+      }
+    } else {
+      ans +=
+          ds[i + 1] - (d + 2) - count(d + 2, ds[i + 1], S[d]) + N - ds[i + 1];
+    }
+  }
+  cout << ans << endl;
+  return;
+}
+
+int main() {
+  fastio();
+  solve();
+  // int t; cin >> t; while(t--)solve();
+
+  // int t; cin >> t;
+  // for(int i=1;i<=t;i++){
+  //   cout << "Case #" << i << ": ";
+  //   solve();
+  // }
+  return 0;
+}
