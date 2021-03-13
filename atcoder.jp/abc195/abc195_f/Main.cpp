768a2af4c737b9cdec5579dc38c5d4c6656d4631
okura <okuraofvegetable@gmail.com>
2021-03-13 22:07:11 +0900

    [AC] abc195 abc195_f

---
+++ atcoder.jp/abc195/abc195_f/Main.cpp
@@ -0,0 +1,188 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define eps 1e-9
+#define INF 2000000000              // 2e9
+#define LLINF 2000000000000000000ll // 2e18 (llmax:9e18)
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+
+#define rep(i, x) for (int i = 0; i < (int)(x); i++)
+#define drep(i, x) for (int i = ((int)(x)-1); i >= 0; i--)
+
+#define popcount __builtin_popcount
+#define next __next
+#define prev __prev
+
+#ifndef LOCAL
+#define dmp(...) ;
+#else
+#define dmp(...)                                                               \
+  cerr << "[ " << #__VA_ARGS__ << " ] : " << dump_str(__VA_ARGS__) << endl
+#endif
+
+// ---------------- Utility ------------------
+
+template <class T>
+bool chmin(T &a, const T &b) {
+  if (a <= b) return false;
+  a = b;
+  return true;
+}
+template <class T>
+bool chmax(T &a, const T &b) {
+  if (a >= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+// ----------------- Input -------------------
+
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  return is >> p.first >> p.second;
+}
+
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+
+// ----------------- Output ------------------
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  return os << p.first << ',' << p.second;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  for (const T &e : v) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const deque<T> &d) {
+  for (const T &e : d) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  return os << "}";
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &m) {
+  os << "{ ";
+  for (const auto &[key, val] : m) os << "( " << key << " -> " << val << " ) ";
+  return os << "}";
+}
+
+template <class TupleTy, size_t... I>
+void dump_tuple(ostream &os, const TupleTy t, std::index_sequence<I...>) {
+  (..., (os << (I == 0 ? "" : ",") << std::get<I>(t)));
+}
+
+template <class... Args>
+ostream &operator<<(ostream &os, const tuple<Args...> &t) {
+  os << "(";
+  dump_tuple(os, t, std::make_index_sequence<sizeof...(Args)>());
+  return os << ")";
+}
+
+void dump_str_rec(ostringstream &) {}
+
+template <class Head, class... Tail>
+void dump_str_rec(ostringstream &oss, Head &&head, Tail &&... tail) {
+  oss << ", " << head;
+  dump_str_rec(oss, forward<Tail>(tail)...);
+}
+
+template <class T, class... U>
+string dump_str(T &&arg, U &&... args) {
+  ostringstream oss;
+  oss << arg;
+  dump_str_rec(oss, forward<U>(args)...);
+  return oss.str();
+}
+
+// --------------- Fast I/O ------------------
+
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+// ------------ End of template --------------
+
+#define endl "\n"
+using ll = long long;
+using pii = pair<int, int>;
+
+void solve() {
+  ll A, B;
+  cin >> A >> B;
+  ll N = B - A + 1;
+  vector<int> ps({2,  3,  5,  7,  11, 13, 17, 19, 23, 29,
+                  31, 37, 41, 43, 47, 53, 59, 61, 67, 71});
+  int M = ps.size();
+  vector<int> ms;
+
+  for (ll i = A; i <= B; i++) {
+    int mask = 0;
+    for (int j = 0; j < ps.size(); j++) {
+      if (i % ps[j] == 0) mask |= (1 << j);
+    }
+    ms.push_back(mask);
+  }
+
+  auto dp = vect(N, vect((1 << M), -1ll));
+
+  function<ll(int, int)> rec = [&](int v, int S) {
+    if (v == N) {
+      if (S == 0)
+        return 1ll;
+      else
+        return 0ll;
+    }
+    if (dp[v][S] != -1ll) return dp[v][S];
+    ll res = 0ll;
+    if ((S & ms[v]) == ms[v]) res += rec(v + 1, (S ^ ms[v]));
+    res += rec(v + 1, S);
+    return dp[v][S] = res;
+  };
+
+  ll ans = 0ll;
+  for (int i = 0; i < (1 << M); i++) { ans += rec(0, i); }
+  cout << ans << endl;
+  return;
+}
+
+int main() {
+  fastio();
+  solve();
+  // int t; cin >> t; while(t--)solve();
+
+  // int t; cin >> t;
+  // for(int i=1;i<=t;i++){
+  //   cout << "Case #" << i << ": ";
+  //   solve();
+  // }
+  return 0;
+}
