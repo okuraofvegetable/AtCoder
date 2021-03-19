5fa209db24f0ed4879d4631b08bc4d3e6cb3fe47
okura <okuraofvegetable@gmail.com>
2021-03-20 03:15:08 +0900

    [AC] agc034 agc034_c

---
+++ atcoder.jp/agc034/agc034_c/Main.cpp
@@ -0,0 +1,202 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define eps 1e-9
+#define INF 2000000000              // 2e9
+#define LLINF 2000000000000000000ll // 2e18 (llmax:9e18)
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+
+#define rep(i, x) for (int i = 0; i < (int)(x); i++)
+#define drep(i, x) for (int i = ((int)(x)-1); i >= 0; i--)
+
+#define popcount __builtin_popcount
+#define next __next
+#define prev __prev
+
+#ifndef LOCAL
+#define dmp(...) ;
+#else
+#define dmp(...)                                                               \
+  cerr << "[ " << #__VA_ARGS__ << " ] : " << dump_str(__VA_ARGS__) << endl
+#endif
+
+// ---------------- Utility ------------------
+
+template <class T>
+bool chmin(T &a, const T &b) {
+  if (a <= b) return false;
+  a = b;
+  return true;
+}
+template <class T>
+bool chmax(T &a, const T &b) {
+  if (a >= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+// ----------------- Input -------------------
+
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  return is >> p.first >> p.second;
+}
+
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+
+// ----------------- Output ------------------
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  return os << p.first << ',' << p.second;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  for (const T &e : v) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const deque<T> &d) {
+  for (const T &e : d) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  return os << "}";
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &m) {
+  os << "{ ";
+  for (const auto &[key, val] : m) os << "( " << key << " -> " << val << " ) ";
+  return os << "}";
+}
+
+template <class TupleTy, size_t... I>
+void dump_tuple(ostream &os, const TupleTy t, std::index_sequence<I...>) {
+  (..., (os << (I == 0 ? "" : ",") << std::get<I>(t)));
+}
+
+template <class... Args>
+ostream &operator<<(ostream &os, const tuple<Args...> &t) {
+  os << "(";
+  dump_tuple(os, t, std::make_index_sequence<sizeof...(Args)>());
+  return os << ")";
+}
+
+void dump_str_rec(ostringstream &) {}
+
+template <class Head, class... Tail>
+void dump_str_rec(ostringstream &oss, Head &&head, Tail &&... tail) {
+  oss << ", " << head;
+  dump_str_rec(oss, forward<Tail>(tail)...);
+}
+
+template <class T, class... U>
+string dump_str(T &&arg, U &&... args) {
+  ostringstream oss;
+  oss << arg;
+  dump_str_rec(oss, forward<U>(args)...);
+  return oss.str();
+}
+
+// --------------- Fast I/O ------------------
+
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+// ------------ End of template --------------
+
+#define endl "\n"
+using ll = long long;
+using pii = pair<int, int>;
+
+void solve() {
+  ll N, X;
+  cin >> N >> X;
+  vector<ll> b(N), l(N), u(N);
+  rep(i, N) cin >> b[i] >> l[i] >> u[i];
+
+  ll base = 0ll;
+  rep(i, N) base += -l[i] * b[i];
+
+  vector<pair<ll, int>> vs;
+  rep(i, N) vs.emplace_back(l[i] * b[i] + u[i] * (X - b[i]), i);
+  sort(all(vs));
+  reverse(all(vs));
+
+  auto check = [&](ll x) -> bool {
+    ll c = x / X, d = x % X;
+    ll res = -LLINF;
+    vector<int> topk(N, -1);
+    ll topk_sum = 0ll;
+    rep(i, c) {
+      topk[vs[i].second] = i;
+      topk_sum += vs[i].first;
+    }
+
+    rep(i, N) {
+      ll sum = base;
+      sum += l[i] * min(d, b[i]) + u[i] * max(0ll, d - b[i]);
+      if (topk[i] == -1)
+        sum += topk_sum;
+      else {
+        sum += topk_sum;
+        sum -= vs[topk[i]].first;
+        sum += vs[c].first;
+      }
+      chmax(res, sum);
+    }
+    return res >= 0ll;
+  };
+
+  ll L = -1ll, R = N * X;
+  while (R - L > 1ll) {
+    ll mid = (L + R) / 2;
+    if (check(mid))
+      R = mid;
+    else
+      L = mid;
+  }
+
+  cout << R << endl;
+
+  return;
+}
+
+int main() {
+  fastio();
+  solve();
+  // int t; cin >> t; while(t--)solve();
+
+  // int t; cin >> t;
+  // for(int i=1;i<=t;i++){
+  //   cout << "Case #" << i << ": ";
+  //   solve();
+  // }
+  return 0;
+}
