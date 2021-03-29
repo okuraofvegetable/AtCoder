bd8c767f94e866f103d738ea90d3a5c08cf545fa
okura <okuraofvegetable@gmail.com>
2021-03-29 16:51:10 +0900

    [AC] abc197 abc197_f

---
+++ atcoder.jp/abc197/abc197_f/Main.cpp
@@ -0,0 +1,255 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define eps 1e-9
+#define INF 1000000000              // 2e9
+#define LLINF 2000000000000000000ll // 2e18 (llmax:9e18)
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+
+#define rep(i, x) for (int i = 0; i < (int)(x); i++)
+#define drep(i, x) for (int i = ((int)(x)-1); i >= 0; i--)
+
+#define popcount __builtin_popcount
+#define next __next
+#define prev __prev
+
+#ifndef LOCAL
+#define dmp(...) ;
+#else
+#define dmp(...)                                                               \
+  cerr << "[ " << #__VA_ARGS__ << " ] : " << dump_str(__VA_ARGS__) << endl
+#endif
+
+// ---------------- Utility ------------------
+
+template <class T>
+bool chmin(T &a, const T &b) {
+  if (a <= b) return false;
+  a = b;
+  return true;
+}
+template <class T>
+bool chmax(T &a, const T &b) {
+  if (a >= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+// ----------------- Input -------------------
+
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  return is >> p.first >> p.second;
+}
+
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+
+// ----------------- Output ------------------
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  return os << p.first << ',' << p.second;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  for (const T &e : v) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const deque<T> &d) {
+  for (const T &e : d) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  return os << "}";
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &m) {
+  os << "{ ";
+  for (const auto &[key, val] : m) os << "( " << key << " -> " << val << " ) ";
+  return os << "}";
+}
+
+template <class TupleTy, size_t... I>
+void dump_tuple(ostream &os, const TupleTy t, std::index_sequence<I...>) {
+  (..., (os << (I == 0 ? "" : ",") << std::get<I>(t)));
+}
+
+template <class... Args>
+ostream &operator<<(ostream &os, const tuple<Args...> &t) {
+  os << "(";
+  dump_tuple(os, t, std::make_index_sequence<sizeof...(Args)>());
+  return os << ")";
+}
+
+void dump_str_rec(ostringstream &) {}
+
+template <class Head, class... Tail>
+void dump_str_rec(ostringstream &oss, Head &&head, Tail &&... tail) {
+  oss << ", " << head;
+  dump_str_rec(oss, forward<Tail>(tail)...);
+}
+
+template <class T, class... U>
+string dump_str(T &&arg, U &&... args) {
+  ostringstream oss;
+  oss << arg;
+  dump_str_rec(oss, forward<U>(args)...);
+  return oss.str();
+}
+
+// --------------- Fast I/O ------------------
+
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+// ------------------ ACL --------------------
+
+// #include <atcoder/modint>
+// constexpr int MOD = 998244353;
+// constexpr int MOD = 1000000007;
+// using mint = atcoder::static_modint<MOD>;
+// ostream &operator<<(ostream &os, const mint &v) {
+//   return os << v.val();
+// }
+
+// ------------ End of template --------------
+
+#define endl "\n"
+using ll = long long;
+using pii = pair<int, int>;
+
+template <class Cost = int>
+struct edge {
+  int from, to;
+  Cost cost;
+  edge() {}
+  edge(int from, int to, Cost cost) : from(from), to(to), cost(cost) {}
+
+  // for debug
+  friend ostream &operator<<(ostream &os, const edge &e) {
+    os << e.from << " -> " << e.to << " : " << e.cost << ", ";
+    return os;
+  }
+};
+
+template <class Cost = int>
+struct Graph {
+  int V;
+  vector<vector<edge<Cost>>> G;
+  Graph() {}
+  Graph(int V) : V(V) { G.resize(V); }
+  void add_edge(int a, int b, Cost cost = Cost(0)) {
+    G[a].push_back(edge<Cost>(a, b, cost));
+  }
+  void add_edge_undirected(int a, int b, Cost cost = Cost(0)) {
+    G[a].push_back(edge<Cost>(a, b, cost));
+    G[b].push_back(edge<Cost>(b, a, cost));
+  }
+  size_t size() const { return G.size(); }
+  const vector<edge<Cost>> &operator[](int id) const { return G[id]; }
+
+  // for debug
+  friend ostream &operator<<(ostream &os, const Graph g) {
+    for (int i = 0; i < g.size(); i++) {
+      os << g[i];
+      if (i + 1 < g.size()) os << endl;
+    }
+    return os;
+  }
+};
+
+void solve() {
+  int N, M;
+  cin >> N >> M;
+  vector<vector<edge<int>>> es(30);
+  for (int i = 0; i < M; i++) {
+    int a, b;
+    char c;
+    cin >> a >> b >> c;
+    a--;
+    b--;
+    es[c - 'a'].emplace_back(a, b, 1);
+    es[c - 'a'].emplace_back(b, a, 1);
+  }
+  auto encode = [&](int a, int b) { return a * N + b; };
+  vector<vector<int>> G(N * N);
+  for (int i = 0; i < 30; i++) {
+    for (int j = 0; j < es[i].size(); j++) {
+      for (int k = 0; k < es[i].size(); k++) {
+        edge e = es[i][j];
+        edge f = es[i][k];
+        int From = encode(e.from, f.from);
+        int To = encode(e.to, f.to);
+        G[From].push_back(To);
+      }
+    }
+  }
+  // dmp(G);
+  vector<int> dist(N * N, INF);
+  dist[encode(0, N - 1)] = 0;
+  queue<int> q;
+  q.push(encode(0, N - 1));
+  while (!q.empty()) {
+    int v = q.front();
+    q.pop();
+    for (int u : G[v]) {
+      if (dist[u] != INF) continue;
+      dist[u] = dist[v] + 1;
+      q.push(u);
+    }
+  }
+
+  int ans = INF;
+  for (int i = 0; i < 30; i++) {
+    for (auto e : es[i]) { chmin(ans, dist[encode(e.from, e.to)] * 2 + 1); }
+  }
+
+  for (int i = 0; i < N; i++) { chmin(ans, dist[encode(i, i)] * 2); }
+
+  if (ans == INF)
+    cout << -1 << endl;
+  else
+    cout << ans << endl;
+
+  return;
+}
+
+int main() {
+  fastio();
+  solve();
+  // int t; cin >> t; while(t--)solve();
+
+  // int t; cin >> t;
+  // for(int i=1;i<=t;i++){
+  //   cout << "Case #" << i << ": ";
+  //   solve();
+  // }
+  return 0;
+}
