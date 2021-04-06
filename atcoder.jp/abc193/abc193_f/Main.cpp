b91b442b11452b06ce4a7c13eb9fbd18d5a6dc42
okura <okuraofvegetable@gmail.com>
2021-04-06 17:25:27 +0900

    [AC] abc193 abc193_f

---
+++ atcoder.jp/abc193/abc193_f/Main.cpp
@@ -0,0 +1,211 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define eps 1e-9
+#define INF 2000000000              // 2e9
+#define LLINF 2000000000000000000ll // 2e18 (llmax:9e18)
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+
+#define rep(i, x) for (int i = 0; i < (int)(x); i++)
+#define drep(i, x) for (int i = ((int)(x)-1); i >= 0; i--)
+
+#define popcount __builtin_popcount
+#define next __next
+#define prev __prev
+
+#ifndef LOCAL
+#define dmp(...) ;
+#else
+#define dmp(...)                                                               \
+  cerr << "[ " << #__VA_ARGS__ << " ] : " << dump_str(__VA_ARGS__) << endl
+#endif
+
+// ---------------- Utility ------------------
+
+template <class T>
+bool chmin(T &a, const T &b) {
+  if (a <= b) return false;
+  a = b;
+  return true;
+}
+template <class T>
+bool chmax(T &a, const T &b) {
+  if (a >= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+// ----------------- Input -------------------
+
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  return is >> p.first >> p.second;
+}
+
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+
+// ----------------- Output ------------------
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  return os << p.first << ',' << p.second;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  for (const T &e : v) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const deque<T> &d) {
+  for (const T &e : d) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  return os << "}";
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &m) {
+  os << "{ ";
+  for (const auto &[key, val] : m) os << "( " << key << " -> " << val << " ) ";
+  return os << "}";
+}
+
+template <class TupleTy, size_t... I>
+void dump_tuple(ostream &os, const TupleTy t, std::index_sequence<I...>) {
+  (..., (os << (I == 0 ? "" : ",") << std::get<I>(t)));
+}
+
+template <class... Args>
+ostream &operator<<(ostream &os, const tuple<Args...> &t) {
+  os << "(";
+  dump_tuple(os, t, std::make_index_sequence<sizeof...(Args)>());
+  return os << ")";
+}
+
+void dump_str_rec(ostringstream &) {}
+
+template <class Head, class... Tail>
+void dump_str_rec(ostringstream &oss, Head &&head, Tail &&... tail) {
+  oss << ", " << head;
+  dump_str_rec(oss, forward<Tail>(tail)...);
+}
+
+template <class T, class... U>
+string dump_str(T &&arg, U &&... args) {
+  ostringstream oss;
+  oss << arg;
+  dump_str_rec(oss, forward<U>(args)...);
+  return oss.str();
+}
+
+// --------------- Fast I/O ------------------
+
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+// ------------------ ACL --------------------
+
+// #include <atcoder/modint>
+// constexpr int MOD = 998244353;
+// constexpr int MOD = 1000000007;
+// using mint = atcoder::static_modint<MOD>;
+// ostream &operator<<(ostream &os, const mint &v) {
+//   return os << v.val();
+// }
+
+// ------------ End of template --------------
+
+#include <atcoder/maxflow>
+#define endl "\n"
+using ll = long long;
+using pii = pair<int, int>;
+
+void solve() {
+  int N;
+  cin >> N;
+  vector<string> f(N);
+  cin >> f;
+  int S = N * N, T = N * N + 1;
+  atcoder::mf_graph<int> g(T + 1);
+  vector<int> dx({-1, 0, 1, 0});
+  vector<int> dy({0, -1, 0, 1});
+  int base = 0;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      int id = i * N + j;
+      if ((i + j) % 2 == 0) {
+        if (f[i][j] == 'W') {
+          g.add_edge(S, id, 0);
+          g.add_edge(id, T, 100000);
+        } else if (f[i][j] == 'B') {
+          g.add_edge(S, id, 100000);
+          g.add_edge(id, T, 0);
+        } else {
+          g.add_edge(S, id, 0);
+          g.add_edge(id, T, 0);
+        }
+      } else {
+        if (f[i][j] == 'W') {
+          g.add_edge(S, id, 100000);
+          g.add_edge(id, T, 0);
+        } else if (f[i][j] == 'B') {
+          g.add_edge(S, id, 0);
+          g.add_edge(id, T, 100000);
+        } else {
+          g.add_edge(S, id, 0);
+          g.add_edge(id, T, 0);
+        }
+      }
+      for (int d = 0; d < 4; d++) {
+        int ni = i + dx[d], nj = j + dy[d];
+        if (ni < 0 || ni >= N || nj < 0 || nj >= N) continue;
+        base++;
+        g.add_edge(ni * N + nj, id, 1);
+        g.add_edge(id, ni * N + nj, 1);
+      }
+    }
+  }
+  cout << (base - g.flow(S, T)) / 2 << endl;
+  return;
+}
+
+int main() {
+  fastio();
+  solve();
+  // int t;
+  // cin >> t;
+  // while (t--) solve();
+
+  // int t; cin >> t;
+  // for(int i=1;i<=t;i++){
+  //   cout << "Case #" << i << ": ";
+  //   solve();
+  // }
+  return 0;
+}
