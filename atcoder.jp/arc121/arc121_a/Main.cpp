eefb4b02b672a7e9b8a68d582f6f51a2a8e7ffcc
okura <okuraofvegetable@gmail.com>
2021-05-29 21:54:52 +0900

    [AC] arc121 arc121_a

---
+++ atcoder.jp/arc121/arc121_a/Main.cpp
@@ -0,0 +1,208 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define eps 1e-9
+#define INF 2000000000              // 2e9
+#define LLINF 2000000000000000000ll // 2e18 (llmax:9e18)
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+
+#define rep(i, x) for (int i = 0; i < (int)(x); i++)
+#define drep(i, x) for (int i = ((int)(x)-1); i >= 0; i--)
+
+#define popcount __builtin_popcount
+#define next __next
+#define prev __prev
+
+#ifndef LOCAL
+#define dmp(...) ;
+#else
+#define dmp(...)                                                               \
+  cerr << "[ " << #__VA_ARGS__ << " ] : " << dump_str(__VA_ARGS__) << endl
+#endif
+
+// ---------------- Utility ------------------
+
+template <class T>
+bool chmin(T &a, const T &b) {
+  if (a <= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+bool chmax(T &a, const T &b) {
+  if (a >= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+// ----------------- Input -------------------
+
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  return is >> p.first >> p.second;
+}
+
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+
+// ----------------- Output ------------------
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  return os << p.first << ',' << p.second;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  for (const T &e : v) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const deque<T> &d) {
+  for (const T &e : d) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  return os << "}";
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &m) {
+  os << "{ ";
+  for (const auto &[key, val] : m) os << "( " << key << " -> " << val << " ) ";
+  return os << "}";
+}
+
+template <class TupleTy, size_t... I>
+void dump_tuple(ostream &os, const TupleTy t, std::index_sequence<I...>) {
+  (..., (os << (I == 0 ? "" : ",") << std::get<I>(t)));
+}
+
+template <class... Args>
+ostream &operator<<(ostream &os, const tuple<Args...> &t) {
+  os << "(";
+  dump_tuple(os, t, std::make_index_sequence<sizeof...(Args)>());
+  return os << ")";
+}
+
+void dump_str_rec(ostringstream &) {}
+
+template <class Head, class... Tail>
+void dump_str_rec(ostringstream &oss, Head &&head, Tail &&... tail) {
+  oss << ", " << head;
+  dump_str_rec(oss, forward<Tail>(tail)...);
+}
+
+template <class T, class... U>
+string dump_str(T &&arg, U &&... args) {
+  ostringstream oss;
+  oss << arg;
+  dump_str_rec(oss, forward<U>(args)...);
+  return oss.str();
+}
+
+// --------------- Fast I/O ------------------
+
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+// ------------------ ACL --------------------
+
+// #include <atcoder/modint>
+// constexpr int MOD = 998244353;
+// constexpr int MOD = 1000000007;
+// using mint = atcoder::static_modint<MOD>;
+// ostream &operator<<(ostream &os, const mint &v) {
+//   return os << v.val();
+// }
+
+// ------------ End of template --------------
+
+#define endl "\n"
+using ll = long long;
+using pii = pair<int, int>;
+
+const bool multitest = false;
+
+void solve() {
+  int N;
+  cin >> N;
+  vector<pii> xy(N);
+  vector<pii> x(N), y(N);
+  for (int i = 0; i < N; i++) {
+    cin >> xy[i].first >> xy[i].second;
+    x[i].first = xy[i].first;
+    y[i].first = xy[i].second;
+    x[i].second = y[i].second = i;
+  }
+
+  sort(all(x));
+  sort(all(y));
+
+  int M = min(100, N);
+
+  vector<bool> use(N, false);
+
+  for (int i = 0; i < M; i++) {
+    use[x[i].second] = true;
+    use[x[N - 1 - i].second] = true;
+    use[y[i].second] = true;
+    use[y[N - 1 - i].second] = true;
+  }
+
+  vector<pii> nxy;
+  for (int i = 0; i < N; i++) {
+    if (use[i]) nxy.push_back(xy[i]);
+  }
+
+  vector<int> seq;
+  for (int i = 0; i < nxy.size(); i++) {
+    for (int j = i + 1; j < nxy.size(); j++) {
+      auto [a, b] = nxy[i];
+      auto [c, d] = nxy[j];
+      seq.push_back(max(abs(a - c), abs(b - d)));
+    }
+  }
+  sort(all(seq), greater<int>());
+  cout << seq[1] << endl;
+
+  return;
+}
+
+int main() {
+  fastio();
+  if (!multitest) {
+    solve();
+  } else {
+    cerr << "[Warning] Multi testcase mode on" << endl;
+    int t;
+    cin >> t;
+    while (t--) solve();
+  }
+  return 0;
+}
