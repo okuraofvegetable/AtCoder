54cc8a9bfca9b78d466810e8cfb8d15794ef3cac
okura <okuraofvegetable@gmail.com>
2021-12-11 21:10:29 +0900

    [AC] abc231 abc231_d

---
+++ atcoder.jp/abc231/abc231_d/Main.cpp
@@ -0,0 +1,226 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define eps 1e-9
+#define INF 2000000000              // 2e9
+#define LLINF 2000000000000000000ll // 2e18 (llmax:9e18)
+#define all(x) (x).begin(), (x).end()
+#define sq(x) ((x) * (x))
+
+#define rep(i, x) for (int i = 0; i < (int)(x); i++)
+#define drep(i, x) for (int i = ((int)(x)-1); i >= 0; i--)
+
+#define popcount __builtin_popcount
+#define next __next
+#define prev __prev
+
+#ifndef LOCAL
+#define dmp(...) ;
+#else
+#define dmp(...)                                                               \
+  cerr << "[ " << #__VA_ARGS__ << " ] : " << dump_str(__VA_ARGS__) << endl
+#endif
+
+// ---------------- Utility ------------------
+
+template <class T>
+bool chmin(T &a, const T &b) {
+  if (a <= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+bool chmax(T &a, const T &b) {
+  if (a >= b) return false;
+  a = b;
+  return true;
+}
+
+template <class T>
+using MaxHeap = priority_queue<T>;
+
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+template <class T>
+vector<T> vect(int len, T elem) {
+  return vector<T>(len, elem);
+}
+
+// ----------------- Input -------------------
+
+template <class T, class U>
+istream &operator>>(istream &is, pair<T, U> &p) {
+  return is >> p.first >> p.second;
+}
+
+template <class T>
+istream &operator>>(istream &is, vector<T> &vec) {
+  for (int i = 0; i < vec.size(); i++) is >> vec[i];
+  return is;
+}
+
+// ----------------- Output ------------------
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+  return os << p.first << ',' << p.second;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  for (const T &e : v) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const deque<T> &d) {
+  for (const T &e : d) os << e << " ";
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &s) {
+  os << "{ ";
+  for (const T &e : s) os << e << " ";
+  return os << "}";
+}
+
+template <class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &m) {
+  os << "{ ";
+  for (const auto &[key, val] : m) os << "( " << key << " -> " << val << " ) ";
+  return os << "}";
+}
+
+template <class TupleTy, size_t... I>
+void dump_tuple(ostream &os, const TupleTy t, std::index_sequence<I...>) {
+  (..., (os << (I == 0 ? "" : ",") << std::get<I>(t)));
+}
+
+template <class... Args>
+ostream &operator<<(ostream &os, const tuple<Args...> &t) {
+  os << "(";
+  dump_tuple(os, t, std::make_index_sequence<sizeof...(Args)>());
+  return os << ")";
+}
+
+void dump_str_rec(ostringstream &) {}
+
+template <class Head, class... Tail>
+void dump_str_rec(ostringstream &oss, Head &&head, Tail &&... tail) {
+  oss << ", " << head;
+  dump_str_rec(oss, forward<Tail>(tail)...);
+}
+
+template <class T, class... U>
+string dump_str(T &&arg, U &&... args) {
+  ostringstream oss;
+  oss << arg;
+  dump_str_rec(oss, forward<U>(args)...);
+  return oss.str();
+}
+
+// --------------- Fast I/O ------------------
+
+void fastio() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(20);
+}
+
+// ------------------ ACL --------------------
+
+// #include <atcoder/modint>
+// constexpr int MOD = 998244353;
+// constexpr int MOD = 1000000007;
+// using mint = atcoder::static_modint<MOD>;
+// ostream &operator<<(ostream &os, const mint &v) {
+//   return os << v.val();
+// }
+
+// ------------ End of template --------------
+
+#define endl "\n"
+using ll = long long;
+using pii = pair<int, int>;
+
+const bool multitest = false;
+
+struct UnionFind {
+  vector<int> par, rank, sz;
+  UnionFind(int n) {
+    par.assign(n, 0);
+    rank.assign(n, 0);
+    sz.assign(n, 1);
+    for (int i = 0; i < n; i++) par[i] = i;
+  }
+  int find(const int x) { return (par[x] == x) ? x : (par[x] = find(par[x])); }
+  void unite(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if (x == y) return;
+    if (rank[x] < rank[y]) {
+      par[x] = y;
+      sz[y] += sz[x];
+    } else {
+      par[y] = x;
+      sz[x] += sz[y];
+      if (rank[x] == rank[y]) rank[x]++;
+    }
+  }
+  bool same(const int x, const int y) { return find(x) == find(y); }
+  int size(const int x) { return sz[find(x)]; }
+  vector<vector<int>> components() {
+    int n = par.size();
+    vector<vector<int>> cs;
+    vector<int> idx(n, -1);
+    for (int i = 0; i < n; i++) {
+      if (find(i) != i) continue;
+      idx[i] = cs.size();
+      cs.emplace_back();
+    }
+    for (int i = 0; i < n; i++) {
+      int id = idx[find(i)];
+      cs[id].push_back(i);
+    }
+    return cs;
+  }
+};
+
+void solve() {
+  int N, M;
+  cin >> N >> M;
+
+  UnionFind uf(N);
+  vector<int> deg(N, 0);
+  bool ok = true;
+  for (int i = 0; i < M; i++) {
+    int a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    deg[a]++;
+    deg[b]++;
+    if (deg[a] > 2 || deg[b] > 2) { ok = false; }
+    if (uf.same(a, b)) { ok = false; }
+    uf.unite(a, b);
+  }
+  cout << (ok ? "Yes" : "No") << endl;
+  return;
+}
+
+int main() {
+  fastio();
+  if (!multitest) {
+    solve();
+  } else {
+    cerr << "[Warning] Multi testcase mode on" << endl;
+    int t;
+    cin >> t;
+    while (t--) solve();
+  }
+  return 0;
+}
